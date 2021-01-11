7ad95731ead79ad409937ec298d5b7992562558f
Nathik Azad <nathikazad@gmail.com>
2021-01-11 13:23:01 -0600

    removed initialize and teardown of fb everytime, added test runs to package.json

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,20 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
+const productionFirebase = admin.initializeApp({
+  databaseURL: "https://mezcalmos-31f1c-default-rtdb.firebaseio.com"
+}, "production");
 
-function initializeFirebase(database) {
+const testFirebase = admin.initializeApp({
+  databaseURL: "https://mezcalmos-test.firebaseio.com"
+}, "test");
+
+function getFirebase(database = "production") {
   console.log("initializing db")
-  if (!database || database == "production") {
-    return admin.initializeApp({
-      databaseURL: "https://mezcalmos-31f1c-default-rtdb.firebaseio.com"
-    }, "production");
+  if (database == "production") {
+    return productionFirebase
   } else {
-    return admin.initializeApp({
-      databaseURL: "https://mezcalmos-test.firebaseio.com"
-    }, "test");
+    return testFirebase
   }
 }
 
@@ -21,18 +24,20 @@ const hasura = require ("./helpers/hasura");
 
 // On sign up.
 exports.processSignUp = functions.auth.user().onCreate(async user => {
-  let firebase = initializeFirebase();
+  let firebase = getFirebase();
   await firebase.database().ref(`/users/${user.uid}/info`).set(
     {displayName:user.displayName,
      photo:user.photoURL,
      email:user.email,
      fbuid:user.providerData[0].uid});
-  return hasura.setClaim(user.uid);
+    let response = hasura.setClaim(user.uid);
+  return response
 });
 
 exports.addHasuraClaims = functions.https.onCall(async (data, context) => {
-  let firebase = initializeFirebase();
-  return hasura.setClaim(firebase, context.auth.uid);
+  let firebase = getFirebase();
+  let response = hasura.setClaim(firebase, context.auth.uid);
+  return response
 });
 
 
@@ -40,25 +45,25 @@ exports.requestTaxi = functions.https.onCall(async (data, context) => {
   if(!context.auth) {
     return { status:"Error", errorMessage: "User needs to be signed in"}
   }
-  let firebase = initializeFirebase(data.database);
+  let firebase = getFirebase(data.database);
   let response = await taxi.request(firebase, data, context.auth.uid)
   return response
 });
 
 exports.acceptTaxiOrder = functions.https.onCall(async (data, context) => {
-  let firebase = initializeFirebase(data.database);
+  let firebase = getFirebase(data.database);
   let response = await taxi.accept(firebase, data, context.auth.uid)
   return response
 });
 
 exports.startTaxiRide = functions.https.onCall(async (data, context) => {
-  let firebase = initializeFirebase(data.database);
+  let firebase = getFirebase(data.database);
   let response = await taxi.start(firebase, data, context.auth.uid)
   return response
 });
 
 exports.finishTaxiRide = functions.https.onCall(async (data, context) => {
-  let firebase = initializeFirebase(data.database);
+  let firebase = getFirebase(data.database);
   let response = await taxi.finish(firebase, data, context.auth.uid)
   return response
 });
@@ -68,7 +73,7 @@ exports.requestGrocery = functions.https.onCall(async (data, context) => {
   if(!context.auth) {
     return { status:"Error", errorMessage: "User needs to be signed in"}
   }
-  let firebase = initializeFirebase(data.database);
+  let firebase = getFirebase(data.database);
   let response = await grocery.request(firebase, data, context.auth.uid)
   return response
 });
