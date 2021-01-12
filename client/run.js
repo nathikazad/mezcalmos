105aae96af72c1d127deaf3780b7656c4af392f5
Nathik Azad <nathikazad@gmail.com>
2021-01-11 18:02:20 -0600

    cloud functions reduced two database writes to chat node to one Added new scripts for emulate only functions

---
+++ client/run.js
@@ -0,0 +1,60 @@
+const { execSync } = require('child_process');
+
+var argv = require('yargs/yargs')(process.argv.slice(2)).argv;
+// console.log(argv._.length)
+
+if (argv.command == "emulate") {
+  let fullCommand = "npx parallelshell \""
+  let emulatorCommand = "firebase emulators:start ";
+  if(argv.emulate == "all") {
+    emulatorCommand += "--only functions,auth,database"
+  } else if (argv.emulate == "onlyFunctions") {
+    emulatorCommand += "--only functions "
+  } else {
+    console.log("Invalid emulate field "+argv.emulate+" can be all or onlyFunctions")
+  }
+  emulatorCommand += "\" "
+  console.log(emulatorCommand)
+  fullCommand += emulatorCommand
+  let emulateModeSuffix = ""
+  if (argv.emulate == "onlyFunctions") {
+    emulateModeSuffix = "OnlyFunctions"
+  }
+
+  if (argv.app == "taxis" || argv.app == "all") {
+    fullCommand += `\"vue-cli-service serve --mode emulate${emulateModeSuffix} --port 8081 src/taxis/main.js\" `
+  }
+  if (argv.app == "deliveries" || argv.app == "all") {
+    fullCommand += `\"vue-cli-service serve --mode emulate${emulateModeSuffix} --port 8082 src/deliveries/main.js\" `
+  }
+  if (argv.app == "customers" || argv.app == "all") {
+    fullCommand += `\"vue-cli-service serve --mode emulate${emulateModeSuffix} --port 8080 src/customers/main.js\" `
+  }
+  execSync(fullCommand, {stdio:[0, 1, 2]});
+} else if (argv.command == "deploy") {
+  let fullCommand;
+  let mode = ""
+  let destSuffix = ""
+  if (argv.mode == "test"){
+    mode = "--mode test"
+    destSuffix = "Test"
+  }
+  if (argv.app == "taxis" || argv.app == "all") {
+    fullCommand = `vue-cli-service build ${mode} --dest ../dist/taxis${destSuffix} src/taxis/main.js`
+    execSync(fullCommand, {stdio:[0, 1, 2]});
+  }
+  if (argv.app == "deliveries" || argv.app == "all") {
+    fullCommand = `vue-cli-service build ${mode} --dest ../dist/deliveries${destSuffix} src/deliveries/main.js`
+    execSync(fullCommand, {stdio:[0, 1, 2]});
+  }
+  if (argv.app == "customers" || argv.app == "all") {
+    fullCommand = `npx vue-cli-service build ${mode} --dest ../dist/customers${destSuffix} src/customers/main.js`
+    execSync(fullCommand, {stdio:[0, 1, 2]});
+  }
+  if (argv.app == "all") {
+    fullCommand = `firebase deploy --only hosting:taxis${destSuffix},hosting:customers${destSuffix},hosting:deliveries${destSuffix}`
+  } else {
+    fullCommand = `firebase deploy --only hosting:${argv.app}${destSuffix}`
+  }
+  execSync(fullCommand, {stdio:[0, 1, 2]});
+}
--- client/src/shared/config/firebase.js
+++ client/src/shared/config/firebase.js
@@ -12,25 +12,27 @@ const firebaseConfig = {
   appId: "1:804036698204:web:39b22436cbb4ef633f8699"
 };
 
-if (process.env.VUE_APP_TEST_DB && process.env.VUE_APP_TEST_DB == "true") {
+if (process.env.VUE_APP_TEST_DB == "true") {
   firebaseConfig.databaseURL = "https://mezcalmos-test.firebaseio.com"
-} else {
+} else { // for emulator and production
   firebaseConfig.databaseURL = "https://mezcalmos-31f1c-default-rtdb.firebaseio.com"
 }
-
 firebase.initializeApp(firebaseConfig);
 
-function firebaseInitFunction(fbCallback) {
-  console.log(process.env)
-  // TODO: detect if being served or deployed and add conditionally
-  if (process.env.VUE_APP_EMULATOR && process.env.VUE_APP_EMULATOR == "true") {
-    firebase.functions().useEmulator("localhost", 5001);
+if (process.env.VUE_APP_EMULATE == "true") {
+  firebase.functions().useEmulator("localhost", 5001);
+  firebase.auth().useEmulator('http://localhost:9099/');
+  if (process.env.VUE_APP_TEST_DB !=  "true") {
+    firebase.database().useEmulator("localhost", 9000);
   }
+}
+
+function firebaseInitFunction(fbCallback) {
   firebase.auth().onAuthStateChanged(fbCallback);
 }
 
 export  function cloudCall(name, payload) {
-  if (process.env.VUE_APP_TEST_DB && process.env.VUE_APP_TEST_DB == "true") {
+  if (process.env.VUE_APP_TEST_DB == "true") {
     payload.database = "test"
   } else {
     payload.database = "production"
