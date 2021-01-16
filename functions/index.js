fcb628900e1a868e4ab9598a5c320095d30f8133
Yassine <yassinoubou93@gmial.com>
2021-01-16 13:51:33 +0100

    arrival time

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 // import { GraphQLClient } from 'graphql-request'
 
 let databaseURL = "https://mezcalmos-31f1c-default-rtdb.firebaseio.com"
-if(process.env.FUNCTIONS_EMULATOR == 'true'){
+if (process.env.FUNCTIONS_EMULATOR == 'true') {
   databaseURL = "https://mezcalmos-test.firebaseio.com"
 }
 
@@ -11,17 +11,18 @@ admin.initializeApp({
   databaseURL: databaseURL
 });
 
-const grocery = require ("./helpers/grocery")
-const taxi = require ("./helpers/taxi")
-const hasura = require ("./helpers/hasura");
+const grocery = require("./helpers/grocery")
+const taxi = require("./helpers/taxi")
+const hasura = require("./helpers/hasura");
 
 // On sign up.
 exports.processSignUp = functions.auth.user().onCreate(async user => {
-  await admin.database().ref(`/users/${user.uid}/info`).set(
-    {displayName:user.displayName,
-     photo:user.photoURL,
-     email:user.email,
-     fbuid:user.providerData[0].uid});
+  await admin.database().ref(`/users/${user.uid}/info`).set({
+    displayName: user.displayName,
+    photo: user.photoURL,
+    email: user.email,
+    fbuid: user.providerData[0].uid
+  });
   return hasura.setClaim(user.uid);
 });
 
@@ -55,4 +56,3 @@ exports.requestGrocery = functions.https.onCall(async (data, context) => {
   let response = await grocery.request(admin, data, context.auth.uid)
   return response
 });
-
