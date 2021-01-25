9a091fa58dd3426e300735e075085ea4f070ec3a
Yassine <yassinoubou93@gmial.com>
2021-01-25 14:29:04 +0100

    pwa config

--- client/src/customers/registerServiceWorker.js
+++ client/src/customers/registerServiceWorker.js
@@ -1,32 +1,25 @@
 /* eslint-disable no-console */
 
-import { register } from 'register-service-worker'
 
-if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
-    },
-    registered () {
-      console.log('Service worker has been registered.')
-    },
-    cached () {
-      console.log('Content has been cached for offline use.')
-    },
-    updatefound () {
-      console.log('New content is downloading.')
-    },
-    updated () {
-      console.log('New content is available; please refresh.')
-    },
-    offline () {
-      console.log('No internet connection found. App is running in offline mode.')
-    },
-    error (error) {
-      console.error('Error during service worker registration:', error)
-    }
-  })
+const prod = process.env.NODE_ENV === 'production'
+const test = process.env.NODE_ENV === 'test'
+const shouldSW = 'serviceWorker' in navigator && (prod || test)
+if (shouldSW) {
+
+  navigator.serviceWorker.register(`${process.env.BASE_URL}sw.js`).then((result) => {
+    console.log("Service Worker Registered!", result)
+  }).catch((err) => {
+    console.log("Something went wrong with registration!", err)
+  });
+} else {
+  console.log({
+    test
+  });
+  console.log({
+    shouldSW
+  });
+  console.log({
+    navigator
+  });
+
 }
