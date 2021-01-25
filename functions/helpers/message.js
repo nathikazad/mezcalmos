9ff764727158f3f40e7fca301d7d8b52f4e15f7b
Nathik Azad <nathikazad@gmail.com>
2021-01-25 10:43:36 -0600

    chat notifications bug

--- functions/helpers/message.js
+++ functions/helpers/message.js
@@ -3,7 +3,8 @@ module.exports = { notifyOthers }
 //possible statuses: lookingForDriver, onTheWay, droppedOff
 async function notifyOthers(firebase, params, message) {
   let senderId = message.userId
-  let chat = (await firebase.database().ref(`/chat/${params.orderId}`).once('value')).val();
+  let chat = (await firebase.database().ref(`/chat/${params.chatId}`).once('value')).val();
+  console.log(chat)
   let senderInfo = chat.participants[senderId]
   senderInfo.id = senderId
   delete chat.participants[senderId]
