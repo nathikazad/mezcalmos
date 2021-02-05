67f340d0b02bdd212abd220f1b15cf902871f955
Nathik Azad <nathikazad@gmail.com>
2021-02-05 06:23:57 -0600

    added structure to first test

--- client/test/integrationTests/firstTest.spec.js
+++ client/test/integrationTests/firstTest.spec.js
@@ -1,9 +1,13 @@
 describe('Google', () => {
   beforeAll(async () => {
-    await page.goto('https://google.com');
+    await page.goto('localhost:8080');
   });
 
-  it('should be titled "Google"', async () => {
-    await expect(page.title()).resolves.toMatch('Google');
+  it('User name should be saved', async () => {
+    customerApp.createUser("name", "image")
+    await expect(customerApp.user.name()).resolves.toMatch('name');
+    let userId = customerApp.getuserId()
+    await customerApp.callTaxi("from", "to")
+    await expect(firebase.user(userId).orders.length, 1)
   });
 });
