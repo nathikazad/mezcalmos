d1bf251e2c6a6e2ed340fda03c6222e167fcc23b
Nathik Azad <nathikazad@gmail.com>
2021-03-07 10:03:06 -0600

    Added date sorting to orders

--- client/src/shared/mixins/filters.js
+++ client/src/shared/mixins/filters.js
@@ -8,3 +8,14 @@ export const formatMessage = (value, nbr) => {
     }
 
 }
+
+export const sortOrdersByDate = (orders) => {
+    let newOrders = {}
+    for (let key in orders) {
+        let date = new Date(orders[key].orderTime).toLocaleDateString("en-US").replaceAll('/', '-'); 
+        newOrders[date] = newOrders[date] || {} ;
+        newOrders[date][key] = orders[key]
+    }
+    let dates = Object.keys(newOrders).sort(function(a,b) { return new Date(a) - new Date(b)})
+    return {date:dates, dateSortedOrders: newOrders}
+}
