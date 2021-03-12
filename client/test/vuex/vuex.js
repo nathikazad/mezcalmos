7ae35eb74b0b6f35408b925473ff4b7169e89283
Nathik Azad <nathikazad@gmail.com>
2021-03-11 21:13:27 -0600

    changed to window.app and modifed dev dependencies

--- client/test/vuex/vuex.js
+++ client/test/vuex/vuex.js
@@ -3,12 +3,12 @@ module.exports = {
   // getting return value: https://stackoverflow.com/questions/57570538/how-do-i-return-a-value-from-page-evaluate-in-puppeteer
   dispatch: async (page, action, params) => {
     await page.evaluate(({action, params}) => {
-      document.getElementsByTagName('a')[0].__vue__.$store._actions[action][0](params)
+      window.app.$store._actions[action][0](params)
     }, {action, params});
   },
   get: async (page, param) => {
     return await page.evaluate(({param}) => {
-      return document.getElementsByTagName('a')[0].__vue__.$store.getters[param]
+      return window.app.$store.getters[param]
     }, {param});
   },
 }
