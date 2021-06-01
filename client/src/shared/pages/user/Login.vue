e2a3630787d45949851dd1c6682a8f622c35c0c8
Nathik Azad <nathikazad@gmail.com>
2021-06-01 17:16:06 -0500

    readded twilio

--- client/src/shared/pages/user/Login.vue
+++ client/src/shared/pages/user/Login.vue
@@ -33,7 +33,6 @@
         to="/enterNumber?method=SMS"
         :mode="{ dark: true, bg_SMS: true }"
         class="float_btn flex align_center center"
-        v-if="isTaxi"
       >
         <fa icon="envelope" class="mr-1"></fa>
         {{$t('shared.login.loginWithSms')}}
@@ -65,9 +64,6 @@ export default {
         this.phoneNumberValidator
       );
     },
-    isTaxi() {
-      return this.$store.getters.appName == "taxi"
-    }
   },
   methods: {
     checkCountryCode(e) {
