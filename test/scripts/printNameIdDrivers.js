1b30dae337050259fd69ae0f651a5af7dd971c3b
Nathik Azad <nathikazad@gmail.com>
2021-06-07 10:18:17 -0500

    Removed SMS notifications

--- test/scripts/printNameIdDrivers.js
+++ test/scripts/printNameIdDrivers.js
@@ -10,7 +10,14 @@ async function main() {
   let drivers = (await firebase.database().ref(`/taxiDrivers`).once('value')).val();
   let users = (await firebase.database().ref(`/users`).once('value')).val();
   for (let driverId in drivers) {
+    let driver = drivers[driverId]
     console.log(users[driverId].info.displayName," ",driverId," ", users[driverId].info.taxiNumber)
+    for(let orderId in driver.orders) {
+      let order = driver.orders[orderId]
+      // console.log('\t '+driver.orders[orderId].acceptRideTime)
+      // console.log('\t '+driver.orders[orderId].customer.name)
+      // console.log('\t '+driver.orders[orderId].status)
+    }
   }
   process.exit()
 }
