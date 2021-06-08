6d86380986c4bbe31fa21d9a9169883cb0311688
Nathik Azad <nathikazad@gmail.com>
2021-06-08 06:39:08 -0500

    test script changes

--- test/scripts/printNameIdDrivers.js
+++ test/scripts/printNameIdDrivers.js
@@ -9,16 +9,27 @@ async function main() {
   
   let drivers = (await firebase.database().ref(`/taxiDrivers`).once('value')).val();
   let users = (await firebase.database().ref(`/users`).once('value')).val();
+  let total = 0;
   for (let driverId in drivers) {
-    let driver = drivers[driverId]
-    console.log(users[driverId].info.displayName," ",driverId," ", users[driverId].info.taxiNumber)
-    for(let orderId in driver.orders) {
-      let order = driver.orders[orderId]
-      // console.log('\t '+driver.orders[orderId].acceptRideTime)
-      // console.log('\t '+driver.orders[orderId].customer.name)
-      // console.log('\t '+driver.orders[orderId].status)
+    let displayName = users[driverId].info.displayName
+    if(displayName == "Alejandro Valle" 
+    || displayName == "Prueba Valle "
+    || displayName == "Nathik Azad"
+    || displayName == "Lia valle"
+    || driverId == "6l5S9qNSrMSIkwZRcchUaP8bJsk2"
+    || driverId == "FtvC1uz9i9QtXu5V142Ju1HZ9Ur1"
+    || driverId == "OwL4SMmebKfQRUMrflR9aVsK4jx2") {
+      continue
     }
+    // if(users[driverId].info.taxiNumber) {
+    //   continue
+    // }
+    // if(drivers[driverId].notificationInfo)
+    //   continue
+    console.log(users[driverId].info.displayName)
+    total += 1
   }
+  console.log(total)
   process.exit()
 }
 
