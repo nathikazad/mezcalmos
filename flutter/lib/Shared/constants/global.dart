9e82d14393a10ffea88bd18190b0a57adcf8de8a
x544d <thejokerofgamzer@gmail.com>
2021-06-22 02:00:47 +0100

    Connectivity Strings

---
+++ flutter/lib/Shared/constants/global.dart
@@ -0,0 +1,4 @@
+const String lanhost        = "http://192.168.1.23";
+const String localhost      = "http://localhost";
+const String authPort       = ":9099";
+const String functionPort   = ":5001";
