b628f77536b11a76a8914acb0e2001a240899459
x544d <thejokerofgamzer@gmail.com>
2021-06-23 16:59:31 +0100

    onInit > Attaching some listeners for realtime database changes .. etc

--- flutter/lib/TaxiApp/controllers/orderController.dart
+++ flutter/lib/TaxiApp/controllers/orderController.dart
@@ -1,37 +1,57 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:get/get.dart';
 import 'package:mezcalmos/Shared/controllers/authController.dart';
 import 'package:mezcalmos/TaxiApp/models/Order.dart';
-import 'package:mezcalmos/TaxiApp/services/ApiService.dart';
+import 'package:mezcalmos/TaxiApp/services/database.dart';
 
 
 class OrderController extends GetxController {
 
-  RxList<Order> _orders =  <Order>[].obs; // this is observable which will be constaintly changing in realtime .
-  AuthController _authController = Get.find<AuthController>(); // since it's already injected .
+  RxList<Order> orders            =  <Order>[].obs; // this is observable which will be constaintly changing in realtime .
+  AuthController _authController  = Get.find<AuthController>(); // since it's already injected .
+  DatabaseHelper _databaseHelper  = Get.find<DatabaseHelper>(); // Already Injected in main function
 
 
-  List<Order> get orders => _orders;
+  // testing Function
+  void _InitialFill()
+  { 
+      print("------------------------ INIT -----------------");
+    _databaseHelper.firebaseDatabase.reference().child('users').once().then((DataSnapshot snapshot) {
+        Map<dynamic, dynamic> values = snapshot.value;
+        values.forEach((key, value) {
+          print( "Key[${key}] = ${value.toString()}");
+        });
+    });
 
-
-  // executed once depending on the use state.
-  Future<void> _fillOrders() async
-  {
-    if (_authController.user != null)
-    {
-        print("Current User ---> "+_authController.user.toString()+"\n\n");
-        _orders.value = await ApiService.fetchOrders() as List<Order>;
-    }
-    print("+ Orders filled > "+orders.length.toString());
   }
 
-
   @override
-  void onInit() async {
+  void onInit() {
     super.onInit();
-    // invoking on the onInit callack to pre-fill ordes.
-    await this._fillOrders();
-    print("[OrderController] > onInit callback done .");
+
+    // testing ... still
+    _InitialFill();
+
+
+    // uhm .. well let's just attach some listeners..
+
+    // Added Order!
+    _databaseHelper.firebaseDatabase.reference().child('openOrders/taxi').onChildAdded.listen((event) {
+      orders.add(Order.fromSnapshot(event.snapshot));
+    });
+
+    // Removed Order
+    _databaseHelper.firebaseDatabase.reference().child('openOrders/taxi').onChildRemoved.listen((event) {
+      orders.removeWhere((element) => element.id == event.snapshot.key);
+    });
+
+    //changed Order
+    _databaseHelper.firebaseDatabase.reference().child('openOrders/taxi').onChildChanged.listen((event) {
+      Order _orderChanged = orders.singleWhere((element) => element.id == event.snapshot.key);
+      orders[orders.indexOf(_orderChanged)] = Order.fromSnapshot(event.snapshot);
+    });
+
   }
 
 
