555707c59489886e6b06501840bbb37555e4e196
x544d <thejokerofgamzer@gmail.com>
2021-06-23 16:58:45 +0100

    some edits on global constants

--- flutter/lib/Shared/constants/global.dart
+++ flutter/lib/Shared/constants/global.dart
@@ -1,4 +1,8 @@
+const String db = "mezcalmos-31f1c-default-rtdb";
+
 const String lanhost        = "http://192.168.1.23";
-const String localhost      = "http://localhost";
+const String localhost      = "http://127.0.0.1";
+
 const String authPort       = ":9099";
+const String dbRoot         = ":9000/?ns=${db}";
 const String functionPort   = ":5001";
