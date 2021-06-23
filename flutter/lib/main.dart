4635a9d456ed8ebbab53df01814cc3d967fa3ed8
x544d <thejokerofgamzer@gmail.com>
2021-06-23 16:57:44 +0100

    DatabaseHelper Injection on main !

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -3,12 +3,14 @@ import 'dart:io' show Platform;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:mezcalmos/Shared/bindings/authBinding.dart';
 import 'package:mezcalmos/Shared/constants/global.dart';
 import 'package:mezcalmos/TaxiApp/routes/SimpleRouter.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:mezcalmos/TaxiApp/services/database.dart';
 
 // TODO :  singing in, view orders, accept orders 
 
@@ -21,18 +23,28 @@ Future<void> main() async {
   String _host = localhost;
 
   try {
-    if ( Platform.isAndroid || Platform.isIOS) _host = lanhost;
+    if ( Platform.isAndroid || Platform.isIOS) _host = lanhost; // IP on LOCAL AREA NETWORK
   } 
-  catch (e) 
+  catch (e)
   {
-    print("+ Using http://localhost as host.");
+    print("+ Using http://localhost/ as host.");
   }
 
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  FirebaseApp _app              = await Firebase.initializeApp();
+  FirebaseDatabase firebaseDb   = FirebaseDatabase(app: _app , databaseURL: _host+dbRoot);
+
+    //   print("1 HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+    // DataSnapshot snap = await firebaseDb.reference().child('users').once().asStream().first;
+    // print(snap.value);
+    //   print("2 HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+
   await FirebaseAuth.instance.useEmulator(_host+authPort);
   FirebaseFunctions.instance.useFunctionsEmulator(origin: _host+functionPort);
   await GetStorage.init();
+
+  Get.put(DatabaseHelper(firebaseDb , _app , _host+dbRoot )); // we can specify after if we have many Databases ..
 
   switch (startPoint) 
   {
