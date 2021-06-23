c43b448b77f3b0e4e3165f8593df5ebabeea87c8
Nathik Azad <nathikazad@gmail.com>
2021-06-23 13:15:47 -0500

    ios working

--- flutter/lib/Shared/constants/global.dart
+++ flutter/lib/Shared/constants/global.dart
@@ -1,6 +1,6 @@
 const String db = "mezcalmos-31f1c-default-rtdb";
 
-const String lanhost        = "http://192.168.1.23";
+const String lanhost        = "http://0.0.0.0";
 const String localhost      = "http://127.0.0.1";
 
 const String authPort       = ":9099";
