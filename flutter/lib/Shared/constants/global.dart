75006d6ad8c19386129dd880acd59eabc9b0988a
x544d <thejokerofgamzer@gmail.com>
2021-06-24 03:14:44 +0100

    Added Some Shared::Constants !

--- flutter/lib/Shared/constants/global.dart
+++ flutter/lib/Shared/constants/global.dart
@@ -1,8 +1,34 @@
+
+// ----------------- CONNECTIVITY STRINGS ----------------- //
+
 const String db = "mezcalmos-31f1c-default-rtdb";
 
-const String lanhost        = "http://0.0.0.0";
-const String localhost      = "http://127.0.0.1";
+const String lanhost                    = "http://192.168.1.23";
+const String localhost                  = "http://127.0.0.1";
 
-const String authPort       = ":9099";
-const String dbRoot         = ":9000/?ns=${db}";
-const String functionPort   = ":5001";
+const String authPort                   = ":9099";
+const String dbRoot                     = ":9000/?ns=${db}";
+const String functionPort               = ":5001";
+
+// ----------------- TEST STRINGS ----------------- //
+
+const String tEmailTestValue            = "driver.one@mezcalmos.com";
+const String tEmailTestPassword         = "password";
+
+// ----------------- Costants Numbers ----------------- //
+
+const double nDefaultMezcalmosTextSize  = 25;
+
+// ----------------- Costants Strings ----------------- //
+
+const String tMez                       = "MEZ";
+const String tCalmos                    = "CALMOS";
+const String tMezcalmos                 = "MEZCALMOS";
+
+const String tConnectMessage            = "Connect using Facebook or using OTP !";
+const String tFacebookSigning           = "Sign in with Facebook";
+const String tSmsSigning                = "Sign in with SMS";
+
+// ----------------- Assets Strings ----------------- //
+
+const String aLogoPath                  = "assets/images/logo.png";
