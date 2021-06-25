4202a5cddf37809baea117de9f6d46b6378733e1
x544d <thejokerofgamzer@gmail.com>
2021-06-25 07:44:38 +0100

    cleaned the code!

--- flutter/lib/TaxiApp/pages/Wrapper.dart
+++ flutter/lib/TaxiApp/pages/Wrapper.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:mezcalmos/Shared/controllers/authController.dart';
+import 'package:mezcalmos/TaxiApp/models/User.dart';
 import 'package:mezcalmos/TaxiApp/pages/AuthScreens/SignInScreen.dart';
 import 'package:mezcalmos/TaxiApp/pages/HomeScreen.dart';
 
@@ -9,7 +10,17 @@ class Wrapper extends GetWidget<AuthController> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() => Get.find<AuthController>().user != null ? HomeScreen() : SignIn());
+    return Obx((){
+      dynamic _user = Get.find<AuthController>().user;
+
+      if ( _user != null )
+      {
+        print("Initialized Wrapper::AuthController::user >> "+ _user.displayName);
+        return HomeScreen();
+      }
+      else return SignIn();
+
+    });
   }
   
 }
