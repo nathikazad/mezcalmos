28bed7ef2176d3039eaf8e107121779fa6fdf581
x544d <thejokerofgamzer@gmail.com>
2021-06-25 07:41:11 +0100

    InjectionHelper > Added some prints

--- flutter/lib/TaxiApp/helpers/InjectionHelper.dart
+++ flutter/lib/TaxiApp/helpers/InjectionHelper.dart
@@ -7,11 +7,14 @@ class TaxiInjectionHelper
   static void toInjectAtSignIn()
   {
     // stuff to inject on SignIn!
+    print("toInjectAtSignIn -> called");
   }
 
 
   static void revokeListenersOnSignOut()
   {
+    print("revokeListenersOnSignOut -> called");
+
     Get.find<OrderController>().dettahListeners();
     print("Disposing the OrderController and Revoking all the ::taxiOpenOrdersNode:: Listners !");
   }
