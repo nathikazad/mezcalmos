64a590187c95ce69e352ba709165d23c77830b6c
x544d <thejokerofgamzer@gmail.com>
2021-06-26 03:09:21 +0100

    Removed unecessary imports !

--- flutter/lib/TaxiApp/pages/Wrapper.dart
+++ flutter/lib/TaxiApp/pages/Wrapper.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:mezcalmos/Shared/controllers/authController.dart';
-import 'package:mezcalmos/TaxiApp/models/User.dart';
 import 'package:mezcalmos/TaxiApp/pages/AuthScreens/SignInScreen.dart';
 import 'package:mezcalmos/TaxiApp/pages/HomeScreen.dart';
 
