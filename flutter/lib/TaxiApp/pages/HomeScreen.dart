33cd677dfe9347aec8e1b8c6c9160baee7a15da7
x544d <thejokerofgamzer@gmail.com>
2021-06-26 03:08:33 +0100

    Using TaxiInjectionHelper.toInjectAtHome() , SideMenuIntegration , Removing Orders List Native Glow!

--- flutter/lib/TaxiApp/pages/HomeScreen.dart
+++ flutter/lib/TaxiApp/pages/HomeScreen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mezcalmos/Shared/constants/global.dart';
 import 'package:mezcalmos/Shared/controllers/authController.dart';
+import 'package:mezcalmos/Shared/controllers/sideMenuDrawer.dart';
 import 'package:mezcalmos/Shared/utilities/GlobalUtilities.dart';
+import 'package:mezcalmos/Shared/widgets/MezcalmosSideMenu.dart';
 import 'package:mezcalmos/Shared/widgets/MezcalmosSwitch.dart';
 import 'package:mezcalmos/Shared/widgets/UsefullWidgets.dart';
 import 'package:mezcalmos/TaxiApp/controllers/orderController.dart';
+import 'package:mezcalmos/TaxiApp/helpers/InjectionHelper.dart';
 
-class HomeScreen extends GetWidget<AuthController> {
+class HomeScreen extends StatelessWidget {
+
 
   @override
   Widget build(BuildContext context) 
@@ -14,14 +19,25 @@ class HomeScreen extends GetWidget<AuthController> {
     final sw    = MediaQuery.of(context).size.width;
     final sh    = MediaQuery.of(context).size.height;
 
+    // ----------------------DI-----------------------
 
-    // Injecting OrderController at this Node.
-    OrderController _orderController  = Get.put(OrderController());
+    // INJECTING OUR NEEDED CONTROLLERS
 
+    TaxiInjectionHelper.toInjectAtHome();
+    
+    // USING FIND TO INITILIZE THE CONTROLLERS INJECTED USING LAZYPUT!
+
+    OrderController           _orderController            = Get.find<OrderController>();
+    SideMenuDraweController   _sideMenuDrawerController   = Get.find<SideMenuDraweController>();
+
+    // ------------------------------------------------
+    //    
     return Scaffold(
+      key: _sideMenuDrawerController.scaffoldKey,
+      drawer: MezcalmosSideMenu(),
+      
       backgroundColor: Colors.white,
-      appBar: MezcalmosSharedWidgets.mezcalmosAppBar(),
-
+      appBar: MezcalmosSharedWidgets.mezcalmosAppBar(_sideMenuDrawerController.openMenu),
       body: Column(
         
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -41,7 +57,7 @@ class HomeScreen extends GetWidget<AuthController> {
                     child: Transform.scale(
                       scale: 1.15,
                       child: Text(
-                        "Incomming Orders", 
+                        tIncommingOrders, 
                         style: TextStyle(
                           fontSize: getSizeRelativeToScreen(70, sw, sh),
                           fontWeight: FontWeight.w400,
@@ -67,114 +83,116 @@ class HomeScreen extends GetWidget<AuthController> {
           Expanded(
             child: Container(
               margin: EdgeInsets.only(left: getSizeRelativeToScreen(40, sw, sh) , right: getSizeRelativeToScreen(40, sw, sh)),
-              child: Obx(() => ListView.builder(
-                  itemCount:  _orderController.orders.length,
-                  itemBuilder: (ctx , i)
-                  {
 
-                    print(" List Len >>>>>>>>>>>... "+_orderController.orders.length.toString());
-                    // return Text(_orderController.orders[i].id);
-                    return GestureDetector(
-                      onTap: () => print("Clicked on order::${_orderController.orders[i].id}"),
-                      child: Container(
-                        color: Colors.transparent,
-                        padding:EdgeInsets.only(top: 10 , bottom: 10) ,
-                        alignment: Alignment.centerLeft,
-                        child: Stack(
-                          clipBehavior: Clip.none,
+              // Removed the Native Glow Effect on list !
+              child: Obx(() => MezcalmosSharedWidgets.MezcalmosNoGlowScrollConfiguration(
+                  ListView.builder(
+                    itemCount:  _orderController.orders.length,
+                    itemBuilder: (ctx , i)
+                    {
+                      // return Text(_orderController.orders[i].id);
+                      return GestureDetector(
+                        onTap: () => print("Clicked on order::${_orderController.orders[i].id}"),
+                        child: Container(
+                          color: Colors.transparent,
+                          padding:EdgeInsets.only(top: 10 , bottom: 10) ,
                           alignment: Alignment.centerLeft,
-                          children: [
-                    
-                            Positioned(
-                              child: CircleAvatar(
-                                maxRadius: 30,
-                                minRadius: 20,
-                                backgroundColor: Colors.grey,
-                                backgroundImage: NetworkImage(_orderController.orders[i].customer['image']),
-                                onBackgroundImageError: (e, s) => print("Failed loading Customer openOrder::id::${_orderController.orders[i].id}"),
-                              ),
-                            ),
-                    
-                            Positioned(
-                              left: 70,
-                              top:5,
-                              child:  Text(_orderController.orders[i].customer['name'] ?? "...",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20
+                          child: Stack(
+                            clipBehavior: Clip.none,
+                            alignment: Alignment.centerLeft,
+                            children: [
+                      
+                              Positioned(
+                                child: CircleAvatar(
+                                  maxRadius: getSizeRelativeToScreen(60, sw, sh),
+                                  minRadius: getSizeRelativeToScreen(59, sw, sh),
+                                  backgroundColor: Colors.grey,
+                                  backgroundImage: NetworkImage(_orderController.orders[i].customer['image']),
+                                  onBackgroundImageError: (e, s) => print("Failed loading Customer openOrder::id::${_orderController.orders[i].id}"),
                                 ),
-                              )
-                            ),
-                    
-                            // info line                          
-                            Positioned(
-                              left: 70,
-                              bottom:10,
-                              child:  Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                    
-                                children: [
-                    
-                                  Icon(Icons.location_on_outlined, size: 15),
-                                  Text(_orderController.orders[i].from['address'].toString().substring(0 , 10)+"... ",
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 1,
-                                    softWrap: false,
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 13
-                                    ),
+                              ),
+                      
+                              Positioned(
+                                left: getSizeRelativeToScreen(150, sw, sh),
+                                top:getSizeRelativeToScreen(10, sw, sh),
+                                child:  Text(_orderController.orders[i].customer['name'] ?? tDefaultCustomerName,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: getSizeRelativeToScreen(40, sw, sh)
                                   ),
-                    
-                                  Icon(Icons.my_location_rounded , size: 15),
-                                  SizedBox(width: 2,),
-                                  Text("0 km",
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 1,
-                                    softWrap: false,
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 13
+                                )
+                              ),
+                      
+                              // info line                          
+                              Positioned(
+                                left: getSizeRelativeToScreen(148, sw, sh),
+                                bottom: getSizeRelativeToScreen(20, sw, sh),
+                                child:  Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                      
+                                  children: [
+                      
+                                    Icon(Icons.location_on_outlined, size: getSizeRelativeToScreen(32, sw, sh)),
+                                    Text(_orderController.orders[i].from['address'].toString().substring(0 , 10)+"... ",
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 1,
+                                      softWrap: false,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize:  getSizeRelativeToScreen(25, sw, sh)
+                                      ),
                                     ),
-                                  ),
-                    
-                    
-                                  SizedBox(width: 5,),
-                                  Icon(Icons.social_distance , size: 15),
-                                  SizedBox(width: 2,),
-                                  Text(_orderController.orders[i].routeInformation['distance']['text'] ?? "~ km",
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 1,
-                                    softWrap: false,
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 13
+                      
+                                    Icon(Icons.my_location_rounded , size: getSizeRelativeToScreen(32, sw, sh)),
+                                    SizedBox(width: 2,),
+                                    Text("0 km",
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 1,
+                                      softWrap: false,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: getSizeRelativeToScreen(25, sw, sh)
+                                      ),
                                     ),
-                                  ),
-                    
-                                  Icon(Icons.timer , size: 15),
-                                  SizedBox(width: 2,),
-                                  Text(_orderController.orders[i].routeInformation['duration']['text'] ?? "? mins",
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 1,
-                                    softWrap: false,
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 13
+                      
+                      
+                                    SizedBox(width: 5,),
+                                    Icon(Icons.social_distance , size: getSizeRelativeToScreen(32, sw, sh)),
+                                    SizedBox(width: 2,),
+                                    Text(_orderController.orders[i].routeInformation['distance']['text'] ?? "? km",
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 1,
+                                      softWrap: false,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: getSizeRelativeToScreen(25, sw, sh)
+                                      ),
                                     ),
-                                  ),
-                                ]                           
-                              )
-                            ),
-                    
-                          ],
+                      
+                                    Icon(Icons.timer , size: getSizeRelativeToScreen(32, sw, sh)),
+                                    SizedBox(width: 2,),
+                                    Text(_orderController.orders[i].routeInformation['duration']['text'] ?? "? mins",
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 1,
+                                      softWrap: false,
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: getSizeRelativeToScreen(25, sw, sh)
+                                      ),
+                                    ),
+                                  ]                           
+                                )
+                              ),
+                      
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-          
-                  }
-                )
+                      );
+                        
+                    }
+                  ),
+              )
               ),
             ),
 
