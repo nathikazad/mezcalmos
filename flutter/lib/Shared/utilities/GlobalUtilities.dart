7940673c5a62e4e0ca5e93acb35ac309b422002f
x544d <thejokerofgamzer@gmail.com>
2021-06-27 18:03:56 +0100

    Default black Snackbar !

--- flutter/lib/Shared/utilities/GlobalUtilities.dart
+++ flutter/lib/Shared/utilities/GlobalUtilities.dart
@@ -1,3 +1,16 @@
 
 // Usefull when trying to make Sizes adptable!
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
 double getSizeRelativeToScreen(double v, double screenHeight, double screenWidth) => (screenHeight / screenWidth) * v;
+
+//Our default snackBar
+void mezcalmosSnackBar(String _title, String _msg) => Get.snackbar
+( 
+  _title , _msg,
+  colorText: Colors.white,
+  backgroundColor: Colors.black87,
+  snackPosition: SnackPosition.BOTTOM,
+  snackStyle: SnackStyle.FLOATING
+);
