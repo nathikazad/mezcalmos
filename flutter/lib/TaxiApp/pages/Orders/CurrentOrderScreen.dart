fec3b071ba04439da185c5d1d3c3757ab217008b
x544d <thejokerofgamzer@gmail.com>
2021-06-27 18:05:55 +0100

    ~

--- flutter/lib/TaxiApp/pages/Orders/CurrentOrderScreen.dart
+++ flutter/lib/TaxiApp/pages/Orders/CurrentOrderScreen.dart
@@ -5,7 +5,7 @@ import 'package:mezcalmos/TaxiApp/controllers/currentOrderController.dart';
 class CurrentOrderScreen extends GetView<CurrentOrderController> {
   @override
   Widget build(BuildContext context) {
-    return Obx(() => controller.id == null ? Center(child: CircularProgressIndicator()) : Center(child: Container(
+    return Obx(() => controller.id == null  || controller.waitingResponse ? Center(child: CircularProgressIndicator()) : Center(child: Container(
 
       alignment: Alignment.center,
       color: Colors.white,
@@ -18,21 +18,21 @@ class CurrentOrderScreen extends GetView<CurrentOrderController> {
           SizedBox(height: 20,),
           Flexible(child: Text(controller.id)),
           SizedBox(height: 50,),
-          TextButton(onPressed: () async => await controller.cancelTaxi("Testing purposes !"), child: Text("CANCEL TAXI" , style: TextStyle(color: Colors.white , fontSize: 20),),
+          TextButton(onPressed: () async  =>  await   controller.cancelTaxi("Testing purposes !"), child: Text("CANCEL TAXI" , style: TextStyle(color: Colors.white , fontSize: 20),),
             style: ButtonStyle(
               fixedSize: MaterialStateProperty.all(Size(200, 50)) ,
               backgroundColor: MaterialStateProperty.all(Colors.black)
             ),
           ),
           SizedBox(height: 20,),
-          TextButton(onPressed: () async => await controller.startRide(), child: Text("START RIDE" , style: TextStyle(color: Colors.white , fontSize: 20),),
+          TextButton(onPressed: () async  =>  await   controller.startRide(), child: Text("START RIDE" , style: TextStyle(color: Colors.white , fontSize: 20),),
             style: ButtonStyle(
               fixedSize: MaterialStateProperty.all(Size(200, 50)) ,
               backgroundColor: MaterialStateProperty.all(Colors.black)
             ),
           ),
           SizedBox(height: 20,),
-          TextButton(onPressed:  () async => await controller.finishRide() , child: Text("FINISH RIDE" , style: TextStyle(color: Colors.white , fontSize: 20),),
+          TextButton(onPressed:  () async  =>  await controller.finishRide() , child: Text("FINISH RIDE" , style: TextStyle(color: Colors.white , fontSize: 20),),
             style: ButtonStyle(
               fixedSize: MaterialStateProperty.all(Size(200, 50)) ,
               backgroundColor: MaterialStateProperty.all(Colors.black)
