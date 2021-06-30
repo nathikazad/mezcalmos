ab0fa683319523e15c59a5597d670e59247e75ad
x544d <thejokerofgamzer@gmail.com>
2021-06-30 06:59:56 +0100

    Added mapStyle01 asset

--- flutter/lib/TaxiApp/constants/assets.dart
+++ flutter/lib/TaxiApp/constants/assets.dart
@@ -1,4 +1,5 @@
 
 
-const String logo_asset     = "assets/images/logo.png";
-const String header_asset   = "assets/images/header.png";
+const String logo_asset         = "assets/images/logo.png";
+const String header_asset       = "assets/images/header.png";
+const String map_style_asset    = "assets/json/mapStyle01.json";
