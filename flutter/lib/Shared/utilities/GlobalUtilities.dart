994d498f79e94779522927b0a9cf9f761b5226f9
x544d <thejokerofgamzer@gmail.com>
2021-06-30 06:58:31 +0100

    Added GetX callback Logger

--- flutter/lib/Shared/utilities/GlobalUtilities.dart
+++ flutter/lib/Shared/utilities/GlobalUtilities.dart
@@ -3,6 +3,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+  void mezcalmosLogger(String text, {bool isError = false}) => print("[ MEZCALMOS ][ GETX ] $text");
+  
 double getSizeRelativeToScreen(double v, double screenHeight, double screenWidth) => (screenHeight / screenWidth) * v;
 
 //Our default snackBar
