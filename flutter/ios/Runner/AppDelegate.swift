1e67f38b43e4dbb2846980369f06376f5731f5c5
Nathik Azad <nathikazad@gmail.com>
2021-07-05 19:17:44 -0500

    gms services typo and git ignore idea

--- flutter/ios/Runner/AppDelegate.swift
+++ flutter/ios/Runner/AppDelegate.swift
@@ -8,10 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices
-    .provideAPIKey
-    ("AIzaSyCOVuUV0qhw0SbNrQMfMVTBDm-5bJVozYg")
-    
+    GMSServices.provideAPIKey("AIzaSyCOVuUV0qhw0SbNrQMfMVTBDm-5bJVozYg")    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
