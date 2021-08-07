7c787be1881d91e270bacacb7b75463d127b6b19
Nathik Azad <nathikazad@gmail.com>
2021-08-07 12:10:45 -0500

    test device only notification

--- test/scripts/testNotification.js
+++ test/scripts/testNotification.js
@@ -7,22 +7,22 @@ const testFirebase = firebaseAdmin.initializeApp({
   databaseURL: "https://mezcalmos-test.firebaseio.com",
 }, "test")
 
-var fs = require('fs');
-var config = JSON.parse(fs.readFileSync('.runtimeconfig.json', 'utf8'));
-const webpush = require('web-push')
-const accountSid = config.twilio.accountid;
-const authToken = config.twilio.authtoken;
-const client = require('twilio')(accountSid, authToken);
-const sender = require("../../functions/helpers/sender");
-const functions = require('firebase-functions');
-webpush.setVapidDetails(
-    'http://www.mezcalmos.com',
-    config.vapidkeys.public,
-    config.vapidkeys.private
-  )
+// var fs = require('fs');
+// var config = JSON.parse(fs.readFileSync('.runtimeconfig.json', 'utf8'));
+// const webpush = require('web-push')
+// const accountSid = config.twilio.accountid;
+// const authToken = config.twilio.authtoken;
+// const client = require('twilio')(accountSid, authToken);
+// const sender = require("../../functions/helpers/sender");
+// const functions = require('firebase-functions');
+// webpush.setVapidDetails(
+//     'http://www.mezcalmos.com',
+//     config.vapidkeys.public,
+//     config.vapidkeys.private
+//   )
 
 let firebase = productionFirebase;
-let driverId;// = "FtvC1uz9i9QtXu5V142Ju1HZ9Ur1";//8QBM8TRgh0YUAyD0xNmOcdB52kB3";
+// let driverId;// = "FtvC1uz9i9QtXu5V142Ju1HZ9Ur1";//8QBM8TRgh0YUAyD0xNmOcdB52kB3";
 
 async function main() {
   if(!process.argv[2]){
@@ -36,15 +36,24 @@ async function main() {
     process.exit()
   }
 
-  try {
-    await webpush.sendNotification(driver.notificationInfo, JSON.stringify({
-      notificationType: "newOrder",
-      message: "Hay una nueva orden de taxi, vea si puede aceptarla."
-    }))
-  } catch(e) {
-      console.log(e)
-      console.log("web push error ",driverId)
+  let message = {
+    title: "Prueba",
+    body: "Eso es una prueba, gracias."
   }
+  const payload = {
+    notification: message
+  };
+
+  await firebase.messaging().sendToDevice(driver.notificationInfo.deviceNotificationToken, payload)
+  // try {
+  //   await webpush.sendNotification(driver.notificationInfo, JSON.stringify({
+  //     notificationType: "newOrder",
+  //     message: "Hay una nueva orden de taxi, vea si puede aceptarla."
+  //   }))
+  // } catch(e) {
+  //     console.log(e)
+  //     console.log("web push error ",driverId)
+  // }
 
   process.exit()
 }
