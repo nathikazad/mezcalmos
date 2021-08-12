193b314151197d5392c02b800659fba4ed9b18c6
Nathik Azad <nathikazad@gmail.com>
2021-08-12 15:30:55 -0500

    fixed kholoud bugs in sender.js

--- functions/helpers/sender.js
+++ functions/helpers/sender.js
@@ -2,18 +2,25 @@ const axios = require('axios');
 const functions = require('firebase-functions');
 
 const keys = require("./keys").keys();
-// const accountSid = keys.twilio.accountid;
-// const authToken = keys.twilio.authtoken;
-// const client = require('twilio')(accountSid, authToken);
+
+let twilio;
+if (keys.twilio && keys.twilio.accountid && keys.twilio.authtoken) {
+  let accountSid = keys.twilio.accountid;
+  let authToken = keys.twilio.authtoken;
+  twilio = require('twilio')(accountSid, authToken);
+}
+
 
 const webpush = require('web-push')
 const vapidKeys = keys.vapidkeys
 
-webpush.setVapidDetails(
-  'http://www.mezcalmos.com',
-  vapidKeys.public,
-  vapidKeys.private
-)
+if (vapidKeys && vapidKeys.public && vapidKeys.private) {
+  webpush.setVapidDetails(
+    'http://www.mezcalmos.com',
+    vapidKeys.public,
+    vapidKeys.private
+  )
+}
 
 module.exports = {
     sendSMS,
@@ -24,43 +31,42 @@ module.exports = {
 
 async function sendWhatsApp(data) {
   
-    const headers = {
-        'Content-Type': 'application/json',
-        'D360-API-KEY': data.apiKey
+  const headers = {
+    'Content-Type': 'application/json',
+    'D360-API-KEY': data.apiKey
+  }
+
+  const message = {
+    "recipient_type": "individual",
+    "to": data.phoneNumber.replace('+', ''),
+    "type": "text",
+    "text": {
+      "body": data.message
     }
-    
-      const message = {
-        "recipient_type": "individual",
-        "to": data.phoneNumber.replace('+',''),
-        "type": "text",
-        "text": {
-            "body": data.message
-        }
-      }
+  }
 
-      try {
-
-       let response = await axios.post("https://waba-sandbox.360dialog.io/v1/messages", message, headers)
-      } catch (error) {
-        console.log(error)
-          return {
-            status: "Error",
-            errorMessage: error
-          }
-      }
+  try {
+    let response = await axios.post("https://waba-sandbox.360dialog.io/v1/messages", message, headers)
+  } catch (error) {
+    console.log(error)
+    return {
+      status: "Error",
+      errorMessage: error
+    }
+  }
 }
 
 async function sendSMS(data) { 
-    try {
-      await client.messages
-        .create({body: data.message, from: '+16304488781', to: data.phoneNumber})
-      } catch (error) {
-        console.log(error)
-        return {
-          status: "Error",
-          errorMessage: `Message Send Error`
-        }
+  try {
+    await twilio.messages
+      .create({ body: data.message, from: '+16304488781', to: data.phoneNumber })
+  } catch (error) {
+    console.log(error)
+    return {
+      status: "Error",
+      errorMessage: `Message Send Error`
     }
+  }
 }
 
 
