0ee06cec216acf09dc5b866a3ad1fd567c491164
Nathik Azad <nathikazad@gmail.com>
2021-09-06 17:40:23 -0500

    hasura migrate fix

--- test/scripts/hasuraMigrate.js
+++ test/scripts/hasuraMigrate.js
@@ -20,88 +20,143 @@ mutation AddOrder($objects: [orders_insert_input!]!){
 
 const userMutationQuery = gql`
 mutation AddUser($objects: [users_insert_input!]!){
-  insert_users(objects: $objects, on_conflict: {constraint: users_pkey, update_columns: [displayName, photo, driver, taxiNumber]}) {
+  insert_users(objects: $objects, on_conflict: {constraint: users_pkey, update_columns: [uid, displayName, photo]}) {
     returning {
       uid
     }
   }
 }`
 
+const driverMutationQuery = gql`
+mutation AddDriver($objects: [drivers_insert_input!]!){
+  insert_drivers(objects: $objects, on_conflict: {constraint: drivers_pkey, update_columns: [driverId, taxiNumber, location, online]}){
+    returning{
+      driverId
+    }
+  }
+}`
+
+const deleteUsersQuery = gql`
+mutation DeleteUsers {
+  delete_users (where: {}) {
+  	affected_rows
+  }
+}`
+
+const getUsersQuery = gql`
+query MyQuery {
+  users {
+    uid
+  }
+}`
+
 let secret = ""
+let testSecret = "rOTavdgkE13VvHckk2AsK5FEMvkCLx5EriEBQF2GJaRLrCw55gv44uATzneQiEMN"
 
 async function insertOrder(query) {
-  try {
-    const client = new GraphQLClient("https://mezcalmos.hasura.app/v1/graphql", {
-      headers: { 'x-hasura-admin-secret': secret }
-    })
-    const result = await client.request(orderMutationQuery, query)
-    // console.log(result)
-  } catch (e) {
-    console.log(e.response.errors)
-  }
+  return await runQuery(orderMutationQuery, query)
 }
 
 async function insertUser(query) {
+  return await runQuery(userMutationQuery, query)
+}
+
+async function deleteUsers() {
+  return await runQuery(deleteUsersQuery)
+}
+
+async function getUsers() {
+  return await runQuery(getUsersQuery)
+}
+
+
+async function insertDriver(query) {
+  return await runQuery(driverMutationQuery, query)
+}
+
+async function runQuery(mutation, query) {
   try {
-    const client = new GraphQLClient("https://mezcalmos.hasura.app/v1/graphql", {
-      headers: { 'x-hasura-admin-secret': secret }
+    const client = new GraphQLClient("https://testing-mezc.hasura.app/v1/graphql", {
+      headers: { 'x-hasura-admin-secret': testSecret }
     })
-    const result = await client.request(userMutationQuery, query)
-    // console.log(result)
+    const result = await client.request(mutation, query)
   } catch (e) {
-    console.log(e.response.errors)
+    console.log(query)
   }
 }
 
-async function saveFile(){
+async function saveFile() {
   let db = (await firebase.database().ref(`/`).once('value')).val();
-	console.log("finished downloading, starting write");
-	
-	let data = JSON.stringify(db, null, "\t");  
-	fs.writeFileSync('data/db-snapshot.json', data);  
-	console.log("Finished");
+  console.log("finished downloading, starting write");
+
+  let data = JSON.stringify(db, null, "\t");
+  fs.writeFileSync('data/db-snapshot.json', data);
+  console.log("Finished");
 }
 
-async function writeToDB(){
-  
+async function writeToDB() {
+
+  // await deleteUsers()
   let data = JSON.parse(fs.readFileSync('data/db-snapshot.json'));
   let drivers = data.taxiDrivers
   let users = data.users
-  let i = 0
+
   let array = []
   for (let userId in users) {
     let user = users[userId]['info']
-    if(!user)
+    if (!user)
       continue
     let object = {
-      uid:userId,
+      uid: userId,
     }
-    if(user.displayName){
+    if (user.displayName) {
       object.displayName = user.displayName
     }
 
-    if(user.photo){
+    if (user.photo) {
       object.photo = user.photo
     }
+    array.push(object)
+  }
+  await insertUser({ objects: array })
 
-    if(drivers[userId] != null){
-      object.driver = true
-      if(user.taxiNumber) {
-        object.taxiNumber = user.taxiNumber
+
+  let driversArray = []
+  for (let driverId in drivers) {
+    let driver = drivers[driverId]
+    let location = null
+    let online = false
+    let taxiNumber = null
+
+    if (driver.location != null) {
+      let position = { ...driver.location }.position
+      location = {
+        type: "Point",
+        coordinates: [position.lat, position.lng]
       }
     }
-    // console.log(object);
-    i++
-    if(i%50 == 0){
-      await insertUser({objects:array})
-      array = []
-    } else {
-      array.push(object)
+
+    if (driver.state != null) {
+      online = driver.state.isLooking || false;
     }
+
+    let info = users[driverId]['info']
+    if (info != null && info.taxiNumber != null) {
+      taxiNumber = info.taxiNumber
+    }
+
+    let object = {
+      driverId: driverId,
+      taxiNumber: taxiNumber,
+      location: location,
+      online: online
+    }
+    driversArray.push(object)
   }
-  i = 0
-  array = []
+  await insertDriver({ objects: driversArray })
+
   let orders = data.orders.taxi
+  let ordersArray = []
   for (let orderId in orders) {
     let order = orders[orderId]
     if (!order || !order.customer || !order.orderTime)
@@ -137,19 +192,13 @@ async function writeToDB(){
       object.cancellationParty = order.cancelledBy
     if (order.reason)
       object.cancellationReason = order.reason
-    i++
-    if (i % 50 == 0) {
-      await insertOrder({ objects: array })
-      array = []
-    } else {
-      array.push(object)
-    }
+
+
+    ordersArray.push(object)
   }
-  await insertOrder({ objects: array })
-  console.log(i)
+  await insertOrder({ objects: ordersArray })
 }
 
 
 // saveFile()
 writeToDB()
-
