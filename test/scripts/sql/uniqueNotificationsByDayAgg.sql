ba594cd5ba559b4324fb83e50217978cedddea21
Nathik Azad <nathikazad@gmail.com>
2021-09-08 10:41:14 -0500

    made notifications distinct by driverid and day

--- test/scripts/sql/uniqueNotificationsByDayAgg.sql
+++ test/scripts/sql/uniqueNotificationsByDayAgg.sql
@@ -1,18 +1,18 @@
-WITH driversSentByDay AS(SELECT distinct on ("driverId")
+WITH driversSentByDay AS(SELECT distinct on ("driverId", day)
     EXTRACT(day FROM "sentTime") as day,
     "driverId"
     FROM notifications
     WHERE ("sentTime" > '2021-09-01'::date AND "sentTime" < '2021-09-01'::date + interval '1 month')
     GROUP BY 1,2),
 
-driversReceivedByDay AS(SELECT distinct on ("driverId")
+driversReceivedByDay AS(SELECT distinct on ("driverId", day)
     EXTRACT(day FROM "receivedTime") as day,
     "driverId"
     FROM notifications
     WHERE ("receivedTime" > '2021-09-01'::date AND "receivedTime" < '2021-09-01'::date + interval '1 month')
     GROUP BY 1,2),
 
-driversReadByDay AS(SELECT distinct on ("driverId")
+driversReadByDay AS(SELECT distinct on ("driverId", day)
     EXTRACT(day FROM "readTime") as day,
     "driverId"
     FROM notifications
