58e0aa781564b92a3dfdb2f1e8181be336beaa95
Nathik Azad <nathikazad@gmail.com>
2021-09-10 15:11:43 -0500

    Side menu flex issue fixed

--- flutter/lib/Shared/widgets/MezSideMenu.dart
+++ flutter/lib/Shared/widgets/MezSideMenu.dart
@@ -8,6 +8,7 @@ import 'package:mezcalmos/Shared/controllers/sideMenuDraweController.dart';
 import 'package:mezcalmos/Shared/utilities/GlobalUtilities.dart';
 import 'package:mezcalmos/Shared/utilities/MezIcons.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:mezcalmos/Shared/constants/global.dart';
 
 class MezSideMenu extends GetWidget<AuthController> {
   SideMenuDraweController _draweController =
@@ -25,111 +26,150 @@ class MezSideMenu extends GetWidget<AuthController> {
 
     return SafeArea(
       child: Drawer(
-        child: Flex(
-          direction: Axis.vertical,
-          children: <Widget>[
-            Container(
-              alignment: Alignment.topLeft,
-              color: Colors.black,
-              height: getSizeRelativeToScreen(25, sw, sh),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                  top: getSizeRelativeToScreen(200, sw, sh),
-                  bottom: getSizeRelativeToScreen(20, sw, sh)),
-              child: SizedBox(
-                height: getSizeRelativeToScreen(300, sw, sh),
-                width: double.infinity,
-                child: CircleAvatar(
-                  // radius: 30.0,
-                  child: ClipOval(
-                    clipBehavior: Clip.antiAlias,
-                    child: controller.user?.image == null ||
-                            controller.user?.image == ""
-                        ? Image.asset(
-                            aDefaultAvatar,
-                            width: getSizeRelativeToScreen(300, sw, sh),
-                            height: getSizeRelativeToScreen(300, sw, sh),
-                            fit: BoxFit.contain,
-                          )
-                        : Image.network(
-                            controller.user!.image! + "?type=large",
-                            fit: BoxFit.cover,
-                            height: getSizeRelativeToScreen(300, sw, sh),
-                            width: getSizeRelativeToScreen(300, sw, sh),
-                          ),
-                  ),
-                  backgroundColor: Colors
-                      .grey.shade100, //Color.fromARGB(255, 222, 222, 222),
-                  // radius: 1,
-                ),
-              ),
-            ),
-            Flexible(
-              child: Text(
-                controller.user?.displayName ?? tDefaultUserName,
-                overflow: TextOverflow.visible,
-                textAlign: TextAlign.center,
-                style: TextStyle(fontFamily: 'psb', fontSize: 25.5),
-              ),
-            ),
-            SizedBox(height: getSizeRelativeToScreen(270, sw, sh)), //
-            ListTile(
-                onTap: () async => await controller.signOut(),
-                leading: Icon(
-                  MezcalmosIcons.power_off,
-                  color: Color.fromARGB(255, 103, 121, 254),
-                  size: 25,
-                ),
-                title: Obx(
-                  () => Text(
-                    lang.strings['shared']['navDrawer']["logout"],
-                    style: TextStyle(fontFamily: 'psb', fontSize: 16),
-                  ),
-                )),
-            ListTile(
-                onTap: () async => await launch(tPrivacyPolicy),
-                leading: Icon(
-                  Icons.lock_sharp,
-                  color: Color.fromARGB(255, 103, 121, 254),
-                  size: 25,
-                ),
-                title: Obx(
-                  () => Text(
-                    lang.strings['shared']['navDrawer']["legal"],
-                    style: TextStyle(fontFamily: 'psb', fontSize: 16),
-                  ),
-                )),
-            Obx(() => ListTile(
-                onTap: () {
-                  lang.changeUserLanguage();
-                  _draweController.cloeseMenu();
-                },
-                leading: Container(
-                  height: 31,
-                  width: 31,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image:
-                          DecorationImage(image: AssetImage(lang.oppositFlag))),
-                ),
-                title: Text(lang.oppositToLang,
-                    style: TextStyle(fontFamily: 'psb', fontSize: 16)))),
-            SizedBox(height: Get.height * 0.2), //
-
+        child: Column(
+          children: [
             Expanded(
               child: Container(
-                alignment: Alignment.bottomLeft,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 15),
-                  child: Text(
-                    'v$VERSION' + (lmd != "prod" ? " $lmd" : " "),
-                  ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                        child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          // radius: 30.0,
+                          Container(
+                            height: 120,
+                            width: 120,
+                            child: ClipOval(
+                              clipBehavior: Clip.antiAlias,
+                              child: controller.user?.image == null ||
+                                      controller.user?.image == ""
+                                  ? Image.asset(
+                                      aDefaultAvatar,
+                                      width:
+                                          getSizeRelativeToScreen(300, sw, sh),
+                                      height:
+                                          getSizeRelativeToScreen(300, sw, sh),
+                                      fit: BoxFit.contain,
+                                    )
+                                  : Image.network(
+                                      controller.user!.image! + "?type=large",
+                                      fit: BoxFit.cover,
+                                      height:
+                                          getSizeRelativeToScreen(300, sw, sh),
+                                      width:
+                                          getSizeRelativeToScreen(300, sw, sh),
+                                    ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: Get.height * 0.05,
+                          ),
+                          Container(
+                            child: Text(
+                              controller.user?.displayName ?? tDefaultUserName,
+                              textAlign: TextAlign.center,
+                              style:
+                                  TextStyle(fontFamily: 'psb', fontSize: 25.5),
+                            ),
+                          )
+                        ],
+                      ),
+                    )),
+                    Container(
+                      width: Get.width * 0.8,
+                      child: Column(
+                        children: [
+                          ListTile(
+                              onTap: () async => await controller.signOut(),
+                              leading: Icon(
+                                MezcalmosIcons.power_off,
+                                color: Color.fromARGB(255, 103, 121, 254),
+                                size: 25,
+                              ),
+                              title: Obx(
+                                () => Text(
+                                  lang.strings['shared']['navDrawer']["logout"],
+                                  style: TextStyle(
+                                      fontFamily: 'psb', fontSize: 16),
+                                ),
+                              )),
+                          ListTile(
+                              onTap: () async => await launch(tPrivacyPolicy),
+                              leading: Icon(
+                                Icons.lock_sharp,
+                                color: Color.fromARGB(255, 103, 121, 254),
+                                size: 25,
+                              ),
+                              title: Obx(
+                                () => Text(
+                                  lang.strings['shared']['navDrawer']["legal"],
+                                  style: TextStyle(
+                                      fontFamily: 'psb', fontSize: 16),
+                                ),
+                              )),
+                          Obx(() => ListTile(
+                              onTap: () {
+                                lang.changeUserLanguage();
+                                _draweController.cloeseMenu();
+                              },
+                              leading: Container(
+                                height: 31,
+                                width: 31,
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    image: DecorationImage(
+                                        image: AssetImage(lang.oppositFlag))),
+                              ),
+                              title: Text(lang.oppositToLang,
+                                  style: TextStyle(
+                                      fontFamily: 'psb', fontSize: 16)))),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: Get.height * 0.12,
+              child: Center(
+                child: Text(
+                  'v1.0.8' + (lmd != "prod" ? " $lmd" : " "),
                 ),
               ),
             )
           ],
         ),
+        // child: Flex(
+        //   direction: Axis.vertical,
+        //   children: <Widget>[
+        //     Container(
+        //       alignment: Alignment.topLeft,
+        //       color: Colors.black,
+        //       height: getSizeRelativeToScreen(25, sw, sh),
+        //     ),
+        //     Padding(
+        //       padding: EdgeInsets.only(
+        //           top: getSizeRelativeToScreen(200, sw, sh),
+        //           bottom: getSizeRelativeToScreen(20, sw, sh)),
+        //       child: SizedBox(
+        //         height: getSizeRelativeToScreen(300, sw, sh),
+        //         width: double.infinity,
+        //     child:
+        //       ),
+        //     ),
+        //     Flexible(
+        //    child:
+        //     ),
+        //     SizedBox(height: getSizeRelativeToScreen(300, sw, sh)),
+
+        //   ],
+        // ),
       ),
     );
   }
