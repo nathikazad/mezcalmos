4995f2028d01f7b6bf5d82547251188ae46f8c08
Amrani Saad <amrani.saad@ofppt-edu.ma>
2021-10-20 20:16:50 +0100

    missplaced the Get.find , fixed

--- flutter/lib/TaxiApp/middlewares/notificationsMiddleware.dart
+++ flutter/lib/TaxiApp/middlewares/notificationsMiddleware.dart
@@ -1,19 +1,26 @@
 import 'package:get/get.dart';
 import 'package:mezcalmos/Shared/controllers/fbNotificationsController.dart';
+import 'package:mezcalmos/Shared/utilities/GlobalUtilities.dart';
 import 'package:mezcalmos/TaxiApp/controllers/taxiAuthController.dart';
 import 'package:mezcalmos/TaxiApp/router.dart';
 
 class TaxiNotificationsMiddleware extends GetMiddleware {
-  final _fbNotifCtrl = Get.find<FBNotificationsController>();
-
   @override
   int? get priority => super.priority;
 
   @override
   GetPageBuilder? onPageBuildStart(GetPageBuilder? page) {
+    mezDbgPrint(
+        "TaxiNotificationsMiddleware ================================== outside.");
+
     if (Get.find<TaxiAuthController>().taxiState?.currentOrder == null) {
       // remove them out.
-      _fbNotifCtrl.clearAllNotification();
+
+      mezDbgPrint(
+          "TaxiNotificationsMiddleware ================================== inside.");
+      // clearing everything because we are using that middleware for both ordersNotif and newMsgNotifs
+      // but still we can go custom and if it's a newMessage Notif we call clearAllMessageNotification() isntead.
+      Get.find<FBNotificationsController>().clearAllNotification();
       // intercept the building of the Widget.
       Future.microtask(
           () => Get.until((route) => route.settings.name == kOrdersListPage));
