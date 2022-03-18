4ddf614e84eef306fee3083f2c7fe1c49cdf5257
Montassare <montassarezekri@gmail.com>
2022-03-18 02:17:47 +0100

    laundry fix

--- flutter/lib/DeliveryAdminApp/pages/Orders/LaundryOrder/Components/LaundryOrderCustomer.dart
+++ flutter/lib/DeliveryAdminApp/pages/Orders/LaundryOrder/Components/LaundryOrderCustomer.dart
@@ -12,14 +12,13 @@ class LaundryOrderCustomer extends StatelessWidget {
   LaundryOrderCustomer({Key? key, required this.order}) : super(key: key);
 
   dynamic _i18n() => Get.find<LanguageController>().strings["DeliveryAdminApp"]
-      ["pages"]
-  ["Orders"]["LaundryOrder"]["Components"]["LaundryOrderCustomer"];
+      ["pages"]["Orders"]["LaundryOrder"]["Components"]["LaundryOrderCustomer"];
 
   LaundryOrderController controller = Get.find<LaundryOrderController>();
 
   @override
   Widget build(BuildContext context) {
-    final txt = Theme.of(context).textTheme;
+    final TextTheme txt = Theme.of(context).textTheme;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -72,25 +71,25 @@ class LaundryOrderCustomer extends StatelessWidget {
                             color: Colors.white,
                           ),
                         ),
-                        Obx(
-                          () => controller.orderHaveNewMessageNotifications(
-                                  order.orderId)
-                              ? Positioned(
-                                  left: 27,
-                                  top: 10,
-                                  child: Container(
-                                    width: 13,
-                                    height: 13,
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10),
-                                        border: Border.all(
-                                            color: const Color(0xfff6efff),
-                                            width: 2),
-                                        color: const Color(0xffff0000)),
-                                  ),
-                                )
-                              : Container(),
-                        )
+                        // Obx(
+                        //   () => controller.orderHaveNewMessageNotifications(
+                        //           order.orderId)
+                        //       ? Positioned(
+                        //           left: 27,
+                        //           top: 10,
+                        //           child: Container(
+                        //             width: 13,
+                        //             height: 13,
+                        //             decoration: BoxDecoration(
+                        //                 borderRadius: BorderRadius.circular(10),
+                        //                 border: Border.all(
+                        //                     color: const Color(0xfff6efff),
+                        //                     width: 2),
+                        //                 color: const Color(0xffff0000)),
+                        //           ),
+                        //         )
+                        //       : Container(),
+                        // )
                       ],
                     ),
                   ),
