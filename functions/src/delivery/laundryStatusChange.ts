b6f1dcd77440b849436b8005cfc4314339137410
Nathik Azad <nathikazad@gmail.com>
2022-04-05 10:19:19 -0500

    bug fixes

--- functions/src/delivery/laundryStatusChange.ts
+++ functions/src/delivery/laundryStatusChange.ts
@@ -93,7 +93,7 @@ async function changeStatus(data: any, newStatus: LaundryOrderStatus, auth?: Aut
 
   if (newStatus == LaundryOrderStatus.OtwPickup || newStatus == LaundryOrderStatus.PickedUp
     || newStatus == LaundryOrderStatus.AtLaundry) {
-    if (order.pickupDriver.id != deliveryDriverId) {
+    if (order.pickupDriver?.id != deliveryDriverId) {
       return {
         status: ServerResponseStatus.Error,
         errorMessage: `Order does not belong to this delivery driver`,
@@ -102,7 +102,7 @@ async function changeStatus(data: any, newStatus: LaundryOrderStatus, auth?: Aut
   }
 
   if (newStatus == LaundryOrderStatus.OtwDelivery || newStatus == LaundryOrderStatus.Delivered) {
-    if (order.dropoffDriver.id != deliveryDriverId) {
+    if (order.dropoffDriver?.id != deliveryDriverId) {
       return {
         status: ServerResponseStatus.Error,
         errorMessage: `Order does not belong to this delivery driver`,
