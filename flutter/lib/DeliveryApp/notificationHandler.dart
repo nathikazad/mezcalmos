eb8998f0970019fd15bb72cca8f0749d20093d68
Montassare <montassarezekri@gmail.com>
2022-04-27 01:26:22 +0100

    small fixes

--- flutter/lib/DeliveryApp/notificationHandler.dart
+++ flutter/lib/DeliveryApp/notificationHandler.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:mezcalmos/DeliveryApp/router.dart';
 import 'package:mezcalmos/Shared/controllers/languageController.dart';
+import 'package:mezcalmos/Shared/helpers/PrintHelper.dart';
 import 'package:mezcalmos/Shared/models/Chat.dart';
 import 'package:mezcalmos/Shared/models/Notification.dart';
 import 'package:mezcalmos/Shared/models/Orders/LaundryOrder.dart';
