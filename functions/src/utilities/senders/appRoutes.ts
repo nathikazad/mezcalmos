6e0b10b9e9fb13db666e42cbba91960b8fcd0664
Nathik Azad <nathikazad@gmail.com>
2022-04-27 21:45:02 -0500

    added recipient type to message url when going to customer

--- functions/src/utilities/senders/appRoutes.ts
+++ functions/src/utilities/senders/appRoutes.ts
@@ -12,6 +12,8 @@ export function orderUrl(
       return `/${orderType}Orders/${orderId}`
     case ParticipantType.DeliveryDriver:
       return `/${orderType}Orders/${orderId}`
+    case ParticipantType.Taxi:
+      return `/currentOrder`;
     default:
       return "/";
   }
@@ -27,8 +29,12 @@ export function chatUrl(
     str += `?orderId=${orderId}`
   if (orderType != null && participantType != null)
     str += `&orderLink=${orderUrl(participantType, orderType, orderId!)}`
+  if (participantType == ParticipantType.Customer)
+    str += `&recipientType=${orderType}`;
   return str
 }
+
+
 
 export function taxiIncomingOrderUrl(
   orderId: string): string {
