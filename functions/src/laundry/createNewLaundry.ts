f0b0c82d3c27367f85fe3d13271d67d33d84929c
Nathik Azad <nathikazad@gmail.com>
2022-06-29 10:30:25 -0500

    fixed create laundry cloud function

--- functions/src/laundry/createNewLaundry.ts
+++ functions/src/laundry/createNewLaundry.ts
@@ -1,19 +1,31 @@
 import * as functions from "firebase-functions";
 import { ServerResponseStatus } from "../shared/models/Generic/Generic";
-import { passChecksForLaundry } from "./helper";
 import * as firebase from "firebase-admin";
 import { UserRecord } from "firebase-functions/v1/auth";
 import * as laundryNodes from "../shared/databaseNodes/services/laundry";
 import * as laundryOperatorNodes from "../shared/databaseNodes/operators/operator";
 import { OrderType } from "../shared/models/Generic/Order";
 import { userInfoNode } from "../shared/databaseNodes/root";
+import { checkDeliveryAdmin, isSignedIn } from "../shared/helper/authorizer";
 
 
 export = functions.https.onCall(async (data, context) => {
-  let validationPass = await passChecksForLaundry(data, context.auth);
-  if (!validationPass.ok) {
-    return validationPass.error;
+  let response = await isSignedIn(context.auth)
+  if (response != undefined) {
+    return {
+      ok: false,
+      error: response
+    }
   }
+
+  response = await checkDeliveryAdmin(context.auth!.uid)
+  if (response != undefined) {
+    return {
+      ok: false,
+      error: response
+    };
+  }
+
 
   if (!data.emailIdOrPhoneNumber && !data.laundryName) {
     return {
@@ -24,13 +36,15 @@ export = functions.https.onCall(async (data, context) => {
 
   let user: UserRecord;
   try {
-    user = await firebase.auth().getUserByPhoneNumber(data.phoneNumber);
+    user = await firebase.auth().getUserByPhoneNumber(data.emailIdOrPhoneNumber);
   } catch (a) {
+    console.log("phone number not there");
     let e: any = a;
     if (e.errorInfo.code == "auth/user-not-found") {
       try {
         user = await firebase.auth().getUserByEmail(data.emailIdOrPhoneNumber)
       } catch (a) {
+        console.log("email also not there");
         let e: any = a;
         return {
           status: ServerResponseStatus.Error,
