14e7e5a4062aed2d5856656e5d680f4a5be703cc
x544D <amrani.saad@ofppt-edu.ma>
2022-07-27 15:49:22 +0100

    Small name Import Correction
    reseting the name back to AppBar

--- flutter/lib/CustomerApp/pages/CustomerWrapper.dart
+++ flutter/lib/CustomerApp/pages/CustomerWrapper.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mezcalmos/CustomerApp/components/Appbar.dart';
+import 'package:mezcalmos/CustomerApp/components/AppBar.dart';
 import 'package:mezcalmos/CustomerApp/components/ServicesCard.dart';
 import 'package:mezcalmos/CustomerApp/controllers/laundry/LaundryController.dart';
 import 'package:mezcalmos/CustomerApp/controllers/orderController.dart';
