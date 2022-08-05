b5f39dde32548b1af42e72aea7bf3b613a2c9876
Montassare <montassare6@gmail.com>
2022-08-05 22:29:32 +0100

    Datetime fix

--- flutter/lib/Shared/helpers/DateTimeHelper.dart
+++ flutter/lib/Shared/helpers/DateTimeHelper.dart
@@ -44,7 +44,7 @@ extension parseDateTime on DateTime {
     } else if (now.difference(toLocal()).inHours.abs() > 0) {
       return "${_i18n()["at"]} ${DateFormat("hh:mm a").format(toLocal())}";
     } else {
-      return "${_i18n()["in"]} ${now.difference(toLocal()).inMinutes.abs()} mins";
+      return " ${_i18n()["in"]} ${now.difference(toLocal()).inMinutes.abs()} min";
     }
   }
 
