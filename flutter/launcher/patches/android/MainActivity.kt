cddc336a8ada0c3222a22ea8dff12f162c924f24
x544D <amrani.saad@ofppt-edu.ma>
2022-08-18 12:48:22 +0100

    MainActivity Patching for Stripe - FlutterFragmentActivity

--- flutter/launcher/patches/android/MainActivity.kt
+++ flutter/launcher/patches/android/MainActivity.kt
@@ -2,9 +2,9 @@ package <mez-package-name>
 import android.app.NotificationManager
 import android.content.Context
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
+<kt_imports>
 
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity: <kt_super>() {
   override fun onResume() {
       super.onResume()
       closeAllNotifications();
