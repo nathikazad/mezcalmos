f6d3f701000647496d45ca96eed96d29e0558558
Montassare <montassare6@gmail.com>
2022-09-28 16:28:22 +0100

    fix laundry name and restaurant name caps

--- flutter/lib/Shared/helpers/TextInputHelper.dart
+++ flutter/lib/Shared/helpers/TextInputHelper.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/services.dart';
 
 class UpperCaseTextFormatter extends TextInputFormatter {
-  /// Uppercase the first letter inside a text input
-  /// used as an input formatter inside TextFormField or TextField widgets
   @override
   TextEditingValue formatEditUpdate(
       TextEditingValue oldValue, TextEditingValue newValue) {
     return TextEditingValue(
-      text: capitalize(newValue.text),
+      text: capitalizeAllWordsInFullSentence(newValue.text),
+      // text: capitalizeAllWordsInFullSentence(newValue.text),
       selection: newValue.selection,
     );
   }
 }
 
-String capitalize(String value) {
+String capitalizeAllWordsInFullSentence(String str) {
+  int i;
+  String constructedString = "";
+  for (i = 0; i < str.length; i++) {
+    if (i == 0) {
+      constructedString += str[0].toUpperCase();
+    } else if (str[i - 1] == ' ') {
+      // mandatory to have index>1 !
+      constructedString += str[i].toUpperCase();
+    } else {
+      constructedString += str[i];
+    }
+  }
+  // print('constructed: $constructedString');
+  return constructedString;
+}
+
+String capitalize1Word(String value) {
   if (value.trim().isEmpty) return "";
   return "${value[0].toUpperCase()}${value.substring(1).toLowerCase()}";
 }
