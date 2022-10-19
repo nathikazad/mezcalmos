6f829037c5bc9da16966e577354fc753c3e5160d
Montassare <montassare6@gmail.com>
2022-10-19 11:22:15 +0100

    on search view

--- flutter/lib/CustomerApp/pages/Restaurants/ListRestaurantsScreen/controllers/ListRestaurantController.dart
+++ flutter/lib/CustomerApp/pages/Restaurants/ListRestaurantsScreen/controllers/ListRestaurantController.dart
@@ -90,7 +90,9 @@ class ListRestaurantsController {
         cat.items.forEach((Item item) {
           final Item _item = item;
           _item.restaurant = restaurant;
-          filteredItems.add(_item);
+          if (item.available) {
+            filteredItems.add(_item);
+          }
         });
       });
     });
