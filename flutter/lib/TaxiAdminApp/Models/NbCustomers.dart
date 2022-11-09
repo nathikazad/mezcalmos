d5715bb6ac23867ca77bec581d4aa96677354a26
Nathik <nathikazd@gmail.com>
2022-11-09 16:32:33 +0530

    removed taxi admin app

--- flutter/lib/TaxiAdminApp/Models/NbCustomers.dart
+++
@@ -1,5 +0,0 @@
-class NbCustomers {
-  final int? day;
-  final int? nbCust;
-  NbCustomers({this.day, this.nbCust});
-}
--- flutter/lib/TaxiAdminApp/helpers/formatter.dart
+++
@@ -1,3 +0,0 @@
-String formatDateForHasura(DateTime dt) {
-  return "${dt.year.toString()}-${dt.month.toString().padLeft(2, '0')}-${dt.day.toString().padLeft(2, '0')}";
-}
