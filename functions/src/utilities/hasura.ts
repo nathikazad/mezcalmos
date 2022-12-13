aeb9d8ef0297d4368c0d21d59b5ae2bb55246aec
Nathik <nathikazd@gmail.com>
2022-12-13 17:32:23 +0530

    adding hasura config to fiunctions

--- functions/src/utilities/hasura.ts
+++ functions/src/utilities/hasura.ts
@@ -1,8 +1,17 @@
+import * as functions from "firebase-functions";
 import { Chain } from "../../../hasura/library/src/generated/graphql-zeus";
 export function getHasura(){
+  if (process.env.FUNCTIONS_EMULATOR === "true") {
   return Chain('http://localhost:8080/v1/graphql', {
     headers: {
       "x-hasura-admin-secret": "myadminsecretkey"
     }
   });
+  } else {
+    return Chain(functions.config().hasura.endpoint, {
+      headers: {
+        "x-hasura-admin-secret": functions.config().hasura.secret
+      }
+    });
+  }
 }
