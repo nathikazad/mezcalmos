62a3c4947f470752b00a865bdd8b637eb0dab7d4
Sanchit Uke <sanchituke99@gmail.com>
2023-01-13 20:34:06 +0530

    Restaurant stats

---
+++ hasura/server/migrations/default/1673621877880_restaurant_stats/up.sql
@@ -0,0 +1,44 @@
+
+CREATE TABLE "public"."mez_json" ("json_object" jsonb NOT NULL, PRIMARY KEY ("json_object") );
+
+-- DROP FUNCTION restaurant_orders_by_date;
+CREATE OR REPLACE FUNCTION public.restaurant_orders_by_date(res_id integer default null)
+ RETURNS mez_json
+ LANGUAGE sql
+ STABLE
+AS $function$
+SELECT json_agg(r)
+FROM (
+SELECT
+json_build_object('date', date(order_time),
+'total_orders', count(*),
+'total_cost', sum(totalcost(restaurant_order)),
+'delivered', count(*) FILTER (WHERE status = 'delivered'),
+'cus_cancelled', count(*) FILTER (WHERE status = 'cancelledByCustomer'),
+'adm_cancelled', count(*) FILTER (WHERE status = 'cancelledByAdmin')) as json
+FROM restaurant_order
+WHERE res_id IS NULL OR restaurant_id = res_id
+GROUP BY date(order_time)
+ORDER BY date(order_time) DESC) r
+$function$;
+
+-- DROP FUNCTION restaurant_orders_by_date;
+CREATE OR REPLACE FUNCTION public.restaurant_orders_by_month(res_id integer default null)
+ RETURNS mez_json
+ LANGUAGE sql
+ STABLE
+AS $function$
+SELECT json_agg(r)
+FROM (
+SELECT
+json_build_object('month', TO_CHAR(TO_DATE (extract(month from order_time)::text, 'MM'), 'Month'),
+'total_orders', count(*),
+'total_cost', sum(totalcost(restaurant_order)),
+'delivered', count(*) FILTER (WHERE status = 'delivered'),
+'cus_cancelled', count(*) FILTER (WHERE status = 'cancelledByCustomer'),
+'adm_cancelled', count(*) FILTER (WHERE status = 'cancelledByAdmin')) as json
+FROM restaurant_order
+WHERE res_id IS NULL OR restaurant_id = res_id
+GROUP BY extract(month from order_time)
+ORDER BY extract(month from order_time) DESC) r
+$function$;
