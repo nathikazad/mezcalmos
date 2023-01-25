0abb5d6617bc1692c6f89b24e2508f5b095dd717
Sanchit Uke <sanchituke99@gmail.com>
2023-01-25 22:10:59 +0530

    restaurants and companies by gps functions

--- hasura/server/migrations/default/1674659364303_alter_table_restaurant_restaurant_alter_column_delivery_details_id/down.sql
+++
@@ -1,1 +0,0 @@
-alter table "restaurant"."restaurant" alter column "delivery_details_id" set not null;
--- hasura/server/migrations/default/1674659364303_alter_table_restaurant_restaurant_alter_column_delivery_details_id/up.sql
+++
@@ -1,1 +0,0 @@
-alter table "restaurant"."restaurant" alter column "delivery_details_id" drop not null;
