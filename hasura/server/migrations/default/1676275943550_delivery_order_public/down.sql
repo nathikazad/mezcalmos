f1b0f5219b315961c8fb3d77bf2826fc896450ae
Montassare Zekri <montassare6@gmail.com>
2023-02-13 09:13:57 +0100

    adding delivery order public to db

---
+++ hasura/server/migrations/default/1676275943550_delivery_order_public/down.sql
@@ -0,0 +1,14 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE
+-- OR REPLACE VIEW "delivery"."order_public" AS
+-- SELECT
+--   "order".id AS restaurant_id,
+--   "order".review_id,
+--   "order".order_time,
+--   "order".estimated_food_ready_time,
+--   "order".actual_food_ready_time,
+--   "order".customer_id,
+--   "order".status
+-- FROM
+--   restaurant."order";
