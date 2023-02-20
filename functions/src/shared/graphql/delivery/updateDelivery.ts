2e4e4afd5c8dfc97cc27f57086a99fb07c03224d
Sanchit Uke <sanchituke99@gmail.com>
2023-02-20 23:09:04 +0530

    courier tables and delivery base location

--- functions/src/shared/graphql/delivery/updateDelivery.ts
+++ functions/src/shared/graphql/delivery/updateDelivery.ts
@@ -1,15 +1,9 @@
-import { HttpsError } from "firebase-functions/v1/auth";
 import { getHasura } from "../../../utilities/hasura";
 import { DeliveryOrder, DeliveryServiceProviderType } from "../../models/Generic/Delivery";
 
 export async function updateDeliveryOrderStatus(deliveryOrder: DeliveryOrder) {
   let chain = getHasura();
-  if(deliveryOrder.deliveryId == null) {
-    throw new HttpsError(
-      "internal",
-      "delivery order id not provided"
-    );
-  }
+
   await chain.mutation({
     update_delivery_order_by_pk: [{
       pk_columns: {
@@ -43,4 +37,21 @@ export async function updateDeliveryOrderCompany(deliveryOrderId: number, delive
       }
     }]
   })
+}
+
+export async function updateDeliveryPackageCost(deliveryOrder: DeliveryOrder) {
+  let chain = getHasura();
+
+  await chain.mutation({
+    update_delivery_order_by_pk: [{
+      pk_columns: {
+        id: deliveryOrder.deliveryId
+      }, 
+      _set: {
+        package_cost: deliveryOrder.packageCost
+      }
+    }, { 
+      package_cost: true
+    }]
+  });
 }
