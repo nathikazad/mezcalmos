598f516ba8319d4fcee8ad8094aed83378e5a387
Sanchit Uke <sanchituke99@gmail.com>
2023-02-22 14:43:05 +0530

    adding back existing operator functions

--- functions/src/delivery/addDeliveryOperator.ts
+++ functions/src/delivery/addDeliveryOperator.ts
@@ -1,53 +1,48 @@
-// import { createDeliveryOperator } from "../shared/graphql/delivery/operator/createDeliveryOperator"
-// import { getDeliveryOperators } from "../shared/graphql/delivery/operator/getDeliveryOperator"
-// import { getUser } from "../shared/graphql/user/getUser"
-// import { ParticipantType } from "../shared/models/Generic/Chat"
-// import { NotificationInfo } from "../shared/models/Generic/Generic"
-// import { UserInfo } from "../shared/models/Generic/User"
-// import { AuthorizeOperatorNotification, Notification, NotificationAction, NotificationType } from "../shared/models/Notification"
-// import { pushNotification } from "../utilities/senders/notifyUser"
+import { createDeliveryOperator } from "../shared/graphql/delivery/operator/createDeliveryOperator"
+import { getDeliveryOperators } from "../shared/graphql/delivery/operator/getDeliveryOperator"
+import { getUser } from "../shared/graphql/user/getUser"
+import { ParticipantType } from "../shared/models/Generic/Chat"
+import { UserInfo } from "../shared/models/Generic/User"
+import { AuthorizeOperatorNotification, Notification, NotificationAction, NotificationType } from "../shared/models/Notification"
+import { AddOperatorDetails } from "../shared/operator/addOperator"
+import { pushNotification } from "../utilities/senders/notifyUser"
 
+export async function addDeliveryOperator(operatorUserId: number, addOperatorDetails: AddOperatorDetails) {
 
-// export interface AddOperatorDetails {
-//     deliveryCompanyId: number,
-//     notificationInfo?: NotificationInfo,
-// }
-// export async function addDeliveryOperator(operatorUserId: number, addOperatorDetails: AddOperatorDetails) {
+  let operatorUserInfo: UserInfo = await getUser(operatorUserId);
+  let notification: Notification = {
+    foreground: <AuthorizeOperatorNotification>{
+      newOperatorName: operatorUserInfo.name,
+      newOperatorImage: operatorUserInfo.image,
+      serviceProviderId: addOperatorDetails.serviceProviderId,
+      time: (new Date()).toISOString(),
+      notificationType: NotificationType.AuthorizeOperator,
+      notificationAction: NotificationAction.ShowSnackbarOnlyIfNotOnPage,
+    },
+    background:{
+      en: {
+        title: `Authorize Operator`,
+        body: `An operator named ${operatorUserInfo.name} is requesting to join`
+      },
+      es: {
+        title: `Authorize Operator`,
+        body: `An operator named ${operatorUserInfo.name} is requesting to join`
+      }
+    },
+    linkUrl: `/`
+  }
+  await createDeliveryOperator(operatorUserId, addOperatorDetails)
 
-//   let operatorUserInfo: UserInfo = await getUser(operatorUserId);
-//   let notification: Notification = {
-//     foreground: <AuthorizeOperatorNotification>{
-//       newOperatorName: operatorUserInfo.name,
-//       newOperatorImage: operatorUserInfo.image,
-//       serviceProviderId: addOperatorDetails.deliveryCompanyId,
-//       time: (new Date()).toISOString(),
-//       notificationType: NotificationType.AuthorizeOperator,
-//       notificationAction: NotificationAction.ShowSnackbarOnlyIfNotOnPage,
-//     },
-//     background:{
-//       en: {
-//         title: `Authorize Operator`,
-//         body: `An operator named ${operatorUserInfo.name} is requesting to join`
-//       },
-//       es: {
-//         title: `Authorize Operator`,
-//         body: `An operator named ${operatorUserInfo.name} is requesting to join`
-//       }
-//     },
-//     linkUrl: `/`
-//   }
-//   await createDeliveryOperator(operatorUserId, addOperatorDetails)
-
-//   let operators = await getDeliveryOperators(addOperatorDetails.deliveryCompanyId);
-//   operators.forEach((o) => {
-//     if(o.owner && o.user) {
-//       pushNotification(
-//         o.user.firebaseId, 
-//         notification, 
-//         o.notificationInfo, 
-//         ParticipantType.DeliveryOperator,
-//         o.user.language,
-//       );
-//     }
-//   })
-// }
+  let operators = await getDeliveryOperators(addOperatorDetails.serviceProviderId);
+  operators.forEach((o) => {
+    if(o.owner && o.user) {
+      pushNotification(
+        o.user.firebaseId, 
+        notification, 
+        o.notificationInfo, 
+        ParticipantType.DeliveryOperator,
+        o.user.language,
+      );
+    }
+  })
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -38,6 +38,10 @@ import { cancelLaundryFromCustomer } from "./laundry/cancelLaundryFromCustomer";
 import { changeDeliveryStatus } from "./delivery/statusChange";
 import { addOperator } from "./shared/operator/addOperator";
 import { authorizeOperator } from "./shared/operator/authorizeOperator";
+import { addRestaurantOperator } from "./restaurant/addRestaurantOperator";
+import { authorizeRestaurantOperator } from "./restaurant/authorizeOperator";
+import { addDeliveryOperator } from "./delivery/addDeliveryOperator";
+import { authorizeDeliveryOperator } from "./delivery/authorizeOperator";
 
 if (process.env.FUNCTIONS_EMULATOR === "true") {
   firebase.initializeApp({
@@ -86,8 +90,8 @@ export const restaurant2 = {
   orderPickedUpByCustomer: authenticatedCall((userId, data) => restaurantStatusChange.orderPickedUpByCustomer(userId, data)),
   cancelOrderFromAdmin: authenticatedCall((userId, data) => restaurantStatusChange.cancelOrder(userId, data)),
   cancelOrderFromCustomer: authenticatedCall((userId, data) => cancelOrderFromCustomer(userId, data)),
-  // addRestaurantOperator: authenticatedCall((userId, data) => addRestaurantOperator(userId, data)),
-  // authorizeRestaurantOperator: authenticatedCall((userId, data) => authorizeRestaurantOperator(userId, data)),
+  addRestaurantOperator: authenticatedCall((userId, data) => addRestaurantOperator(userId, data)),
+  authorizeRestaurantOperator: authenticatedCall((userId, data) => authorizeRestaurantOperator(userId, data)),
   // refundCustomerCustomAmount: authenticatedCall((userId, data) => restaurantStatusChange.refundCustomerCustomAmount(userId, data)),
 }
 
@@ -115,8 +119,8 @@ export const laundry = {
 
 export const delivery2 = {
   assignDriver: authenticatedCall((userId, data) => assignDriver(userId, data)),
-  // addDeliveryOperator: authenticatedCall((userId, data) => addDeliveryOperator(userId, data)),
-  // authorizeDeliveryOperator: authenticatedCall((userId, data) => authorizeDeliveryOperator(userId, data)),
+  addDeliveryOperator: authenticatedCall((userId, data) => addDeliveryOperator(userId, data)),
+  authorizeDeliveryOperator: authenticatedCall((userId, data) => authorizeDeliveryOperator(userId, data)),
   changeStatus: authenticatedCall((userId, data) => changeDeliveryStatus(userId, data)),
   
   // restaurantStartDelivery: authenticatedCall((userId, data) => restaurantDelivery.startDelivery(userId, data)),
