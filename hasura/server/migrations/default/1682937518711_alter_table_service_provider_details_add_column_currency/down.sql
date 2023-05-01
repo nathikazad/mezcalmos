d0fa9e9e1152d3d1c4931f54329699fbee1ea005
SanchitUke <sanchituke99@gmail.com>
2023-05-01 16:57:26 +0530

    currency

---
+++ hasura/server/migrations/default/1682937518711_alter_table_service_provider_details_add_column_currency/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "service_provider"."details" add column "currency" text
+--  not null default 'Peso';
---
+++ hasura/server/migrations/default/1682937518711_alter_table_service_provider_details_add_column_currency/up.sql
@@ -0,0 +1,2 @@
+alter table "service_provider"."details" add column "currency" text
+ not null default 'Peso';
