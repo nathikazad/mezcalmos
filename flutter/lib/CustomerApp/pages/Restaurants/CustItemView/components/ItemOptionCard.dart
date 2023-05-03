8fa5f3386bcd6bd8ac1357916e41592fcbe90c8d
khouloud <souchanna3@gmail.com>
2023-05-03 13:14:07 +0100

    the overflow fix

--- flutter/lib/CustomerApp/pages/Restaurants/CustItemView/components/ItemOptionCard.dart
+++ flutter/lib/CustomerApp/pages/Restaurants/CustItemView/components/ItemOptionCard.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mezcalmos/CustomerApp/models/Cart.dart';
@@ -93,22 +95,25 @@ class _ItemOptionCardState extends State<ItemOptionCard> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  Text(
-                    choice.name[userLanguage].toString().inCaps,
-                    style: context.txt.bodyMedium?.copyWith(
-                      color: (widget.cartItem.value!.chosenChoices[optionId]
-                                  ?.contains(choice) ??
-                              false)
-                          ? primaryBlueColor
-                          : null,
-                      fontWeight: (widget
-                                  .cartItem.value!.chosenChoices[optionId]
-                                  ?.contains(choice) ??
-                              false)
-                          ? FontWeight.w700
-                          : null,
+                  Expanded(
+                    child: Text(
+                      choice.name[userLanguage].toString().inCaps,
+                      style: context.txt.bodyMedium?.copyWith(
+                        color: (widget.cartItem.value!.chosenChoices[optionId]
+                                    ?.contains(choice) ??
+                                false)
+                            ? primaryBlueColor
+                            : null,
+                        fontWeight: (widget
+                                    .cartItem.value!.chosenChoices[optionId]
+                                    ?.contains(choice) ??
+                                false)
+                            ? FontWeight.w700
+                            : null,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
                     ),
-                    maxLines: 2,
                   ),
                   SizedBox(
                     width: 5,
