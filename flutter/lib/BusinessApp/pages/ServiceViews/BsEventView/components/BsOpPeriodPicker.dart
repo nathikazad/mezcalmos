785c91bf802c14860800380fc2b4ec7df04c3b38
Montassare Zekri <montassare6@gmail.com>
2023-05-08 09:54:25 +0100

    adding validation to schedule and period pickers

--- flutter/lib/BusinessApp/pages/ServiceViews/BsEventView/components/BsOpPeriodPicker.dart
+++ flutter/lib/BusinessApp/pages/ServiceViews/BsEventView/components/BsOpPeriodPicker.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:mezcalmos/Shared/constants/global.dart';
-import 'package:mezcalmos/Shared/helpers/PrintHelper.dart';
 import 'package:mezcalmos/Shared/models/Utilities/Period.dart';
 import 'package:mezcalmos/Shared/models/Utilities/Schedule.dart';
 import 'package:mezcalmos/Shared/widgets/MezPeriodPicker/MezPeriodPicker.dart';
@@ -16,47 +16,61 @@ class BsOpPeriodPicker extends StatelessWidget {
   final void Function(PeriodOfTime) onNewPeriodSelected;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          "Time",
-        ),
-        smallSepartor,
-        Card(
-          child: InkWell(
-            onTap: () {
-              showModalBottomSheet<PeriodOfTime>(
-                  context: context,
-                  isScrollControlled: true,
-                  builder: (BuildContext ctx) {
-                    return MezPeriodPicker(
-                      startDate: DateTime.now(),
-                      numberOfDaysInterval: 7,
-                      periodOfTime: null,
-                      serviceSchedule: scheduleFromData(defaultSchedule),
-                    );
-                  }).then((PeriodOfTime? value) {
-                if (value != null) {
-                  mezDbgPrint(
-                      "🇹🇿🇹🇿🇹🇿🇹🇿🇹🇿🇹🇿🇹🇿 New Period Selected: $value");
-                  onNewPeriodSelected.call(value);
-                }
-              });
-            },
-            child: Container(
-              padding: const EdgeInsets.all(8),
-              child: Row(
-                children: [
-                  Icon(Icons.watch_later),
-                  SizedBox(width: 5),
-                  Text(timePeriod?.toNormalString() ?? "Time")
-                ],
+    return FormField<PeriodOfTime>(
+        initialValue: timePeriod,
+        validator: (PeriodOfTime? value) {
+          if (value == null) {
+            return "Please select a time";
+          }
+          return null;
+        },
+        builder: (FormFieldState<PeriodOfTime> state) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Time",
               ),
-            ),
-          ),
-        )
-      ],
-    );
+              smallSepartor,
+              Card(
+                child: InkWell(
+                  onTap: () {
+                    showModalBottomSheet<PeriodOfTime>(
+                        context: context,
+                        isScrollControlled: true,
+                        builder: (BuildContext ctx) {
+                          return MezPeriodPicker(
+                            startDate: DateTime.now(),
+                            numberOfDaysInterval: 7,
+                            periodOfTime: null,
+                            serviceSchedule: scheduleFromData(defaultSchedule),
+                          );
+                        }).then((PeriodOfTime? value) {
+                      state.didChange(value);
+                      if (value != null) {
+                        onNewPeriodSelected.call(value);
+                      }
+                    });
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    child: Row(
+                      children: [
+                        Icon(Icons.watch_later),
+                        SizedBox(width: 5),
+                        Text(timePeriod?.toNormalString() ?? "Time")
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 2, left: 12),
+                child: Text(state.errorText ?? "",
+                    style: context.theme.inputDecorationTheme.errorStyle),
+              ),
+            ],
+          );
+        });
   }
 }
--- flutter/lib/BusinessApp/pages/ServiceViews/components/BsOpScheduleSelector.dart
+++ flutter/lib/BusinessApp/pages/ServiceViews/components/BsOpScheduleSelector.dart
@@ -4,7 +4,6 @@ import 'package:mezcalmos/BusinessApp/pages/BsOpSchedulePickerView/BsOpScheduleP
 import 'package:mezcalmos/Shared/cloudFunctions/model.dart';
 import 'package:mezcalmos/Shared/constants/global.dart';
 import 'package:mezcalmos/Shared/helpers/DateTimeHelper.dart';
-import 'package:mezcalmos/Shared/helpers/PrintHelper.dart';
 
 class BsOpScheduleSelector extends StatelessWidget {
   const BsOpScheduleSelector({
@@ -17,78 +16,95 @@ class BsOpScheduleSelector extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          "Schedule",
-          style: context.textTheme.bodyLarge,
-        ),
-        smallSepartor,
-        Card(
-          child: InkWell(
-            onTap: () async {
-              final Schedule? returnedSchedule =
-                  await BsOpSchedulePickerView.navigate(
-                schedule: schedule,
-              );
-              onScheduleSelected.call(returnedSchedule);
-            },
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              child: schedule != null
-                  ? Column(
-                      children: [
-                        ...schedule!.openHours.entries
-                            .map((MapEntry<Weekday, OpenHours> entries) {
-                          final String from = convertToAmPm(
-                              entries.value.from.first.toInt(),
-                              entries.value.from.last.toInt());
-                          final String to = convertToAmPm(
-                              entries.value.to.first.toInt(),
-                              entries.value.to.last.toInt());
-                          final String time = "$from - $to";
-                          return entries.value.isOpen
-                              ? Row(
-                                  children: [
-                                    Icon(
-                                      Icons.calendar_today,
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 8.0),
-                                      child: Text(
-                                        entries.key.name,
-                                      ),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 8.0),
-                                      child: Text(
-                                        time,
-                                      ),
-                                    ),
-                                  ],
-                                )
-                              : SizedBox.shrink();
-                        }),
-                      ],
-                    )
-                  : Row(
-                      children: [
-                        Flexible(
-                          fit: FlexFit.tight,
-                          child: Text(
-                            "Add schedule",
-                            style: context.textTheme.bodyLarge,
+    return FormField<Schedule>(
+        initialValue: schedule,
+        validator: (Schedule? value) {
+          if (value == null) {
+            return "Please select a schedule";
+          }
+          return null;
+        },
+        builder: (FormFieldState<Schedule> state) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Schedule",
+                style: context.textTheme.bodyLarge,
+              ),
+              smallSepartor,
+              Card(
+                child: InkWell(
+                  onTap: () async {
+                    final Schedule? returnedSchedule =
+                        await BsOpSchedulePickerView.navigate(
+                      schedule: schedule,
+                    );
+                    state.didChange(returnedSchedule);
+                    onScheduleSelected.call(returnedSchedule);
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(16),
+                    child: schedule != null
+                        ? Column(
+                            children: [
+                              ...schedule!.openHours.entries
+                                  .map((MapEntry<Weekday, OpenHours> entries) {
+                                final String from = convertToAmPm(
+                                    entries.value.from.first.toInt(),
+                                    entries.value.from.last.toInt());
+                                final String to = convertToAmPm(
+                                    entries.value.to.first.toInt(),
+                                    entries.value.to.last.toInt());
+                                final String time = "$from - $to";
+                                return entries.value.isOpen
+                                    ? Row(
+                                        children: [
+                                          Icon(
+                                            Icons.calendar_today,
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 8.0),
+                                            child: Text(
+                                              entries.key.name,
+                                            ),
+                                          ),
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 8.0),
+                                            child: Text(
+                                              time,
+                                            ),
+                                          ),
+                                        ],
+                                      )
+                                    : SizedBox.shrink();
+                              }),
+                            ],
+                          )
+                        : Row(
+                            children: [
+                              Flexible(
+                                fit: FlexFit.tight,
+                                child: Text(
+                                  "Add schedule",
+                                  style: context.textTheme.bodyLarge,
+                                ),
+                              ),
+                              Icon(Icons.add)
+                            ],
                           ),
-                        ),
-                        Icon(Icons.add)
-                      ],
-                    ),
-            ),
-          ),
-        ),
-      ],
-    );
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 2, left: 12),
+                child: Text(state.errorText ?? "",
+                    style: context.theme.inputDecorationTheme.errorStyle),
+              ),
+            ],
+          );
+        });
   }
 }
