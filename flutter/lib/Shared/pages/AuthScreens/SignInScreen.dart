ae159755c3fde3949555f68755b6a4c59eaa0f60
Montassare Zekri <montassare6@gmail.com>
2023-05-15 15:43:49 +0100

    remove test code

--- flutter/lib/Shared/pages/AuthScreens/SignInScreen.dart
+++ flutter/lib/Shared/pages/AuthScreens/SignInScreen.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: constant_identifier_names
 
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ionicons/ionicons.dart';
@@ -118,19 +121,19 @@ class _SignInViewState extends State<SignInView> {
       ];
     } else {
       return <Widget>[
-        // SizedBox(
-        //   height: 20,
-        // ),
-        // if (!kIsWeb) facebookLoginBtn(lmode),
-        // SizedBox(
-        //   height: 10,
-        // ),
-        // smsLoginBtn(),
-        // SizedBox(
-        //   height: 10,
-        // ),
-        // if (lmode != AppLaunchMode.dev && !kIsWeb && Platform.isIOS)
-        //   appleLoginBtn(),
+        SizedBox(
+          height: 20,
+        ),
+        if (!kIsWeb) facebookLoginBtn(lmode),
+        SizedBox(
+          height: 10,
+        ),
+        smsLoginBtn(),
+        SizedBox(
+          height: 10,
+        ),
+        if (lmode != AppLaunchMode.dev && !kIsWeb && Platform.isIOS)
+          appleLoginBtn(),
         if (MezEnv.appLaunchMode == AppLaunchMode.stage)
           ...stageLoginBtns(stageCredentials),
         if (MezEnv.appLaunchMode == AppLaunchMode.dev)
