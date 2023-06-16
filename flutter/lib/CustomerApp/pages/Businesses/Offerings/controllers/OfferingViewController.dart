3f95a14a7f1c6bd5ce154c27718c38bab5d92aed
chiragkr04 <chiragkumar6921@gmail.com>
2023-06-16 13:07:26 +0530

    fix: offering type for homes

--- flutter/lib/CustomerApp/pages/Businesses/Offerings/controllers/OfferingViewController.dart
+++ flutter/lib/CustomerApp/pages/Businesses/Offerings/controllers/OfferingViewController.dart
@@ -384,7 +384,7 @@ class CustHomeRentalViewController {
           id: _cartId.value,
           businessId: _homeRental.value!.business.id,
           itemId: _homeRental.value!.id!,
-          offeringType: OfferingType.Rental,
+          offeringType: OfferingType.Home,
           time: startDate.value!.toString(),
           parameters: BusinessItemParameters(
             guests: _totalGuests.value,
@@ -400,7 +400,7 @@ class CustHomeRentalViewController {
         BusinessCartItem(
           businessId: _homeRental.value!.business.id,
           itemId: _homeRental.value!.id!,
-          offeringType: OfferingType.Rental,
+          offeringType: OfferingType.Home,
           time: startDate.value!.toString(),
           parameters: BusinessItemParameters(
             guests: _totalGuests.value,
