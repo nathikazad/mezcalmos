e923f62347f0f528724f1c81b6de358ad593cdf8
chiragkr04 <chiragkumar6921@gmail.com>
2023-07-10 13:03:14 +0530

    feat: adding co working with bizapp

--- flutter/lib/BusinessApp/pages/ServiceViews/BsCoWorkingView/BsCoWorkingView.dart
+++ flutter/lib/BusinessApp/pages/ServiceViews/BsCoWorkingView/BsCoWorkingView.dart
@@ -11,6 +11,7 @@ import 'package:mezcalmos/Shared/cloudFunctions/model.dart';
 import 'package:mezcalmos/Shared/constants/global.dart';
 import 'package:mezcalmos/Shared/controllers/languageController.dart';
 import 'package:mezcalmos/Shared/helpers/BusinessHelpers/RentalHelper.dart';
+import 'package:mezcalmos/Shared/helpers/PrintHelper.dart';
 import 'package:mezcalmos/Shared/models/Utilities/Generic.dart';
 import 'package:mezcalmos/Shared/routes/MezRouter.dart';
 import 'package:mezcalmos/Shared/widgets/MezAppBar.dart';
@@ -384,27 +385,75 @@ class _BsCoWorkingViewState extends State<BsCoWorkingView>
                       )),
                 ],
               ),
+              Obx(() {
+                return MezStringDropDown(
+                  validator: (String? value) {
+                    if (viewController.roomType.value == null) {
+                      return _i18n()["coWorking"]["homeTypeError"];
+                    }
+                    return null;
+                  },
+                  langPath: _i18n()["coWorking"],
+                  items: CoWorkingRoomType.values
+                      .map((CoWorkingRoomType e) => e.toFirebaseFormatString())
+                      .toList(),
+                  value:
+                      viewController.roomType.value?.toFirebaseFormatString(),
+                  onChanged: (String? newRoomType) {
+                    if (newRoomType != null) {
+                      viewController.roomType.value =
+                          newRoomType.toCoWorkingRoomType();
+                    }
+                  },
+                  labelText: _i18n()["coWorking"]["homeType"],
+                );
+              }),
               smallSepartor,
-              MezCard(
-                content: Column(
+              Column(
+                children: [
+                  Obx(
+                    () => BsOpOfferingPricesList(
+                      availbleUnits: viewController.avalbleUnits,
+                      onAddPrice: (TimeUnit unit) {
+                        viewController.detailsController
+                            .addPriceTimeUnit(timeUnit: unit);
+                      },
+                      onRemovePrice: (TimeUnit unit) {
+                        viewController.detailsController.removeTimeUnit(unit);
+                      },
+                      seletedPrices:
+                          viewController.detailsController.priceTimeUnitMap,
+                    ),
+                  ),
+                ],
+              ),
+              smallSepartor,
+              if (viewController.roomType.value !=
+                  CoWorkingRoomType.PrivateRoom)
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Obx(
-                      () => BsOpOfferingPricesList(
-                        availbleUnits: viewController.avalbleUnits,
-                        onAddPrice: (TimeUnit unit) {
-                          viewController.detailsController
-                              .addPriceTimeUnit(timeUnit: unit);
-                        },
-                        onRemovePrice: (TimeUnit unit) {
-                          viewController.detailsController.removeTimeUnit(unit);
-                        },
-                        seletedPrices:
-                            viewController.detailsController.priceTimeUnitMap,
+                    smallSepartor,
+                    Text(
+                      "Number of desk",
+                      style: context.textTheme.bodyLarge,
+                    ),
+                    TextField(
+                      controller: viewController.deskController,
+                    ),
+                    smallSepartor,
+                    Text(
+                      "Surface area",
+                      style: context.textTheme.bodyLarge,
+                    ),
+                    TextField(
+                      controller: viewController.areaController,
+                      decoration: InputDecoration(
+                        suffix: Text("Sq ft"),
                       ),
                     ),
                   ],
                 ),
-              ),
             ],
           ),
           _multipleRoomPriceView(),
@@ -422,53 +471,81 @@ class _BsCoWorkingViewState extends State<BsCoWorkingView>
   }
 
   Widget _multipleRoomPriceView() {
-    return Column(
-      children: [
-        ...viewController.additionalRooms.asMap().entries.map(
-          (data) {
-            final int index = data.key;
-            return Column(
-              children: [
-                smallSepartor,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "${_i18n()["coWorking"]["coWorkingType"]} ${index + 2}",
-                      style: context.textTheme.bodyMedium?.copyWith(
-                        fontWeight: FontWeight.w600,
-                        color: Colors.black,
+    return Obx(() {
+      return Column(
+        children: [
+          ...viewController.additionalRooms.asMap().entries.map(
+            (data) {
+              final int index = data.key;
+              return Column(
+                children: [
+                  smallSepartor,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "${_i18n()["coWorking"]["coWorkingType"]} ${index + 2}",
+                        style: context.textTheme.bodyMedium?.copyWith(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black,
+                        ),
                       ),
-                    ),
-                    InkWell(
-                        onTap: () async {
-                          viewController.deleteNewRoom(index);
-                        },
-                        child: Ink(
-                          padding: const EdgeInsets.all(5),
-                          child: Row(
-                            children: [
-                              Icon(
-                                Icons.delete_outline,
-                                size: 20,
-                                color: Colors.red,
-                              ),
-                              SizedBox(
-                                width: 8,
-                              ),
-                              Text(
-                                "Remove",
-                                style: context.textTheme.bodyLarge
-                                    ?.copyWith(color: Colors.red),
-                              ),
-                            ],
-                          ),
-                        )),
-                  ],
-                ),
-                smallSepartor,
-                MezCard(
-                  content: Column(
+                      InkWell(
+                          onTap: () async {
+                            viewController.deleteNewRoom(index);
+                          },
+                          child: Ink(
+                            padding: const EdgeInsets.all(5),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.delete_outline,
+                                  size: 20,
+                                  color: Colors.red,
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Text(
+                                  "Remove",
+                                  style: context.textTheme.bodyLarge
+                                      ?.copyWith(color: Colors.red),
+                                ),
+                              ],
+                            ),
+                          )),
+                    ],
+                  ),
+                  smallSepartor,
+                  Obx(() {
+                    return MezStringDropDown(
+                      validator: (String? value) {
+                        if (viewController.additionalRooms[index]["roomType"] ==
+                            null) {
+                          return _i18n()["coWorking"]["homeTypeError"];
+                        }
+                        return null;
+                      },
+                      langPath: _i18n()["coWorking"],
+                      items: CoWorkingRoomType.values
+                          .map((CoWorkingRoomType e) =>
+                              e.toFirebaseFormatString())
+                          .toList(),
+                      value: viewController.additionalRooms[index]["roomType"],
+                      onChanged: (String? newRoomType) {
+                        if (newRoomType != null) {
+                          viewController.additionalRooms[index]["roomType"] =
+                              newRoomType
+                                  .toCoWorkingRoomType()
+                                  .toFirebaseFormatString();
+                          mezDbgPrint(
+                              "newRoomType: ${viewController.additionalRooms[index]["roomType"]}");
+                        }
+                      },
+                      labelText: _i18n()["coWorking"]["homeType"],
+                    );
+                  }),
+                  Column(
                     children: [
                       Obx(
                         () => BsOpOfferingPricesList(
@@ -493,32 +570,44 @@ class _BsCoWorkingViewState extends State<BsCoWorkingView>
                       ),
                     ],
                   ),
-                ),
-              ],
-            );
-          },
-        ),
-      ],
-    );
-  }
-
-  Widget _singlePriceView() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Obx(
-          () => BsOpOfferingPricesList(
-            availbleUnits: viewController.avalbleUnits,
-            onAddPrice: (TimeUnit unit) {
-              viewController.detailsController.addPriceTimeUnit(timeUnit: unit);
+                  if (viewController.additionalRooms[index]["roomType"]
+                          .toString()
+                          .toCoWorkingRoomType() !=
+                      CoWorkingRoomType.PrivateRoom)
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        smallSepartor,
+                        Text(
+                          "Number of desk",
+                          style: context.textTheme.bodyLarge,
+                        ),
+                        TextField(
+                          controller: viewController.additionalRooms[index]
+                              ["deskController"],
+                          keyboardType: TextInputType.number,
+                        ),
+                        smallSepartor,
+                        Text(
+                          "Surface area",
+                          style: context.textTheme.bodyLarge,
+                        ),
+                        TextField(
+                          controller: viewController.additionalRooms[index]
+                              ["areaController"],
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                            suffix: Text("Sq ft"),
+                          ),
+                        ),
+                      ],
+                    ),
+                ],
+              );
             },
-            onRemovePrice: (TimeUnit unit) {
-              viewController.detailsController.removeTimeUnit(unit);
-            },
-            seletedPrices: viewController.detailsController.priceTimeUnitMap,
           ),
-        ),
-      ],
-    );
+        ],
+      );
+    });
   }
 }
--- flutter/lib/BusinessApp/pages/ServiceViews/BsCoWorkingView/controllers/BsCoWorkingViewController.dart
+++ flutter/lib/BusinessApp/pages/ServiceViews/BsCoWorkingView/controllers/BsCoWorkingViewController.dart
@@ -15,26 +15,6 @@ import 'package:mezcalmos/Shared/models/Services/Business/Business.dart';
 
 typedef OfferingPricesMap = Map<TimeUnit, TextEditingController>;
 
-enum CoWorkingCategory1{
-  DedicatedDesk,
-  PrivateRoom,
-  FullFloorOffice
-}
-
-extension ParseHomeCategory1ToString on CoWorkingCategory1 {
-  String toFirebaseFormatString() {
-    String str = toString().split('.').last;
-    return str[0].toLowerCase() + str.substring(1);
-  }
-}
-
-extension ParseStringToCoWorkingCategory1 on String {
-  CoWorkingCategory1 toCoWorkingCategory1() {
-    return CoWorkingCategory1.values.firstWhere((CoWorkingCategory1 homeCategory1) =>
-        homeCategory1.toFirebaseFormatString().toLowerCase() == toLowerCase());
-  }
-}
-
 class BsCoWorkingViewController {
   // instances //
 
@@ -44,8 +24,7 @@ class BsCoWorkingViewController {
   bool secondFormValid = false;
   BusinessItemDetailsController detailsController =
       BusinessItemDetailsController();
-  TextEditingController bedroomsController = TextEditingController();
-  TextEditingController bathroomsController = TextEditingController();
+  TextEditingController deskController = TextEditingController();
   TextEditingController areaController = TextEditingController();
 
   // vars //
@@ -54,13 +33,13 @@ class BsCoWorkingViewController {
   // state variables //
   Rxn<Home> _rental = Rxn<Home>();
   Rxn<Location> homeLocation = Rxn<Location>();
-  Rxn<CoWorkingCategory1> homeType = Rxn<CoWorkingCategory1>();
+  Rxn<CoWorkingRoomType> homeType = Rxn<CoWorkingRoomType>();
   Rxn<String> petFriendly = Rxn<String>();
   // getters //
   Home? get rental => _rental.value;
   bool get isEditing => _rental.value != null;
 
-  RxList<Map<String, dynamic>> additionalRooms = RxList();
+  RxList<RxMap<String, dynamic>> additionalRooms = RxList();
   Rxn<CoWorkingRoomType> roomType = Rxn<CoWorkingRoomType>();
 
   List<TimeUnit> get _possibleTimeUnits => List.unmodifiable([
@@ -109,14 +88,6 @@ class BsCoWorkingViewController {
       detailsController.clearPrices();
       await detailsController.initEditMode(
           itemDetailsId: rental!.details.id.toInt());
-      bedroomsController.text = rental!.bedrooms.toString();
-      bathroomsController.text = rental!.bathrooms.toString();
-      areaController.text = rental!.details.additionalParameters?["area"]
-              .toString()
-              .replaceAll("sq ft", "")
-              .trim() ??
-          "";
-      petFriendly.value = rental!.details.additionalParameters?["petFriendly"];
       // homeLocation.value = rental!.gpsLocation;
       // homeType.value = rental!.category1;
       final String? roomType1 =
@@ -124,23 +95,26 @@ class BsCoWorkingViewController {
       roomType.value = roomType1?.toCoWorkingRoomType();
       additionalRooms.value = rental!
               .details.additionalParameters?["additionalRooms"]
-              ?.map<Map<String, dynamic>>(
+              ?.map<RxMap<String, dynamic>>(
             (dynamic e) {
               final BusinessItemDetailsController ctrl =
                   BusinessItemDetailsController();
               ctrl.constructNewRoomsCost(
                   constructBusinessServiceCost(e["cost"]));
-              return {
+              return RxMap<String, dynamic>({
                 "roomType": e["roomType"],
                 "controller": ctrl,
-              };
+                "deskController":
+                    TextEditingController(text: e["deskCount"] ?? ""),
+                "areaController": TextEditingController(text: e["area"] ?? ""),
+              });
             },
-          )?.toList() as List<Map<String, dynamic>>? ??
+          )?.toList() as List<RxMap<String, dynamic>>? ??
           [];
     }
   }
 
-  void changeHomeType(CoWorkingCategory1 newHomeType) {
+  void changeHomeType(CoWorkingRoomType newHomeType) {
     homeType.value = newHomeType;
   }
 
@@ -157,10 +131,12 @@ class BsCoWorkingViewController {
   void addNewRoom() {
     final BusinessItemDetailsController ctrl = BusinessItemDetailsController();
     ctrl.addPriceTimeUnit(timeUnit: newRoomAvaiableUnits(ctrl).first);
-    additionalRooms.add({
+    additionalRooms.add(RxMap({
       "roomType": CoWorkingRoomType.FullFloorOffice.toFirebaseFormatString(),
       "controller": ctrl,
-    });
+      "deskController": TextEditingController(),
+      "areaController": TextEditingController(),
+    }));
   }
 
   void deleteNewRoom(int index) {
@@ -175,8 +151,20 @@ class BsCoWorkingViewController {
     final BusinessItemDetails details =
         await detailsController.contructDetails();
     details.additionalParameters = {
-      "area": areaController.text.trim() + " sq ft",
-      "petFriendly": petFriendly.value,
+      "roomType1": roomType.value?.toFirebaseFormatString(),
+      "additionalRooms": [
+        for (final Map<String, dynamic> room in additionalRooms)
+          {
+            "roomType": room["roomType"],
+            "cost": (room["controller"] as BusinessItemDetailsController)
+                .priceTimeUnitMap
+                .map((TimeUnit key, TextEditingController value) => MapEntry(
+                    key.toFirebaseFormatString(), double.parse(value.text))),
+            "deskCount": int.tryParse(
+                (room["deskController"] as TextEditingController).text),
+            "area": (room["areaController"] as TextEditingController).text
+          },
+      ],
     };
     final Home rental = Home(
       availableFor: HomeAvailabilityOption.Rent,
@@ -185,8 +173,6 @@ class BsCoWorkingViewController {
       category1: HomeCategory1.Room,
       // gpsLocation: homeLocation.value,
       details: details,
-      bathrooms: int.tryParse(bathroomsController.text),
-      bedrooms: int.tryParse(bedroomsController.text),
     );
     return rental;
   }
@@ -200,8 +186,6 @@ class BsCoWorkingViewController {
       category1: HomeCategory1.Room,
       availableFor: HomeAvailabilityOption.Rent,
       // gpsLocation: homeLocation.value,
-      bathrooms: int.tryParse(bathroomsController.text),
-      bedrooms: int.tryParse(bedroomsController.text),
       details: detailsController.details!,
     );
     return rental;
@@ -217,17 +201,21 @@ class BsCoWorkingViewController {
           await update_item_additional_params(
             id: rental!.details.id.toInt(),
             additionalParams: {
-              "area": areaController.text.trim() + " sq ft",
-              "petFriendly": petFriendly.value,
               "roomType1": roomType.value?.toFirebaseFormatString(),
               "additionalRooms": [
                 for (final Map<String, dynamic> room in additionalRooms)
                   {
                     "roomType": room["roomType"],
-                    "cost": room["controller"].priceTimeUnitMap.value.map(
-                        (TimeUnit key, TextEditingController value) => MapEntry(
-                            key.toFirebaseFormatString(),
-                            double.parse(value.text))),
+                    "cost":
+                        (room["controller"] as BusinessItemDetailsController)
+                            .priceTimeUnitMap
+                            .map((TimeUnit key, TextEditingController value) =>
+                                MapEntry(key.toFirebaseFormatString(),
+                                    double.parse(value.text))),
+                    "deskCount": int.tryParse(
+                        (room["deskController"] as TextEditingController).text),
+                    "area":
+                        (room["areaController"] as TextEditingController).text
                   },
               ],
             },
