5913d9d6ecbb524aa1566d6fc3359ac0efc274f7
Nathik <nathikazd@gmail.com>
2023-09-04 12:19:19 -0600

    removed agora

--- flutter/lib/Shared/routes/nativeOnlyRoutes.dart
+++ flutter/lib/Shared/routes/nativeOnlyRoutes.dart
@@ -1,6 +1,3 @@
-import 'package:mezcalmos/Shared/pages/AgoraCall.dart';
-import 'package:mezcalmos/Shared/pages/MessagingScreen/NativeMessagingScreen.dart';
-import 'package:mezcalmos/Shared/routes/sharedRoutes.dart';
 import 'package:qlevar_router/qlevar_router.dart';
 
 // Routes Keys.
@@ -9,15 +6,15 @@ import 'package:qlevar_router/qlevar_router.dart';
 class NativeOnlyRoutes {
   static const String kAgoraCallScreenRoute = '/agora';
   static List<QRoute> routes = [
-    QRoute(
-      name: kAgoraCallScreenRoute,
-      path: kAgoraCallScreenRoute,
-      builder: () => AgoraCall(),
-    ),
-    QRoute(
-      name: SharedRoutes.kMessagesRoute,
-      path: SharedRoutes.kMessagesRoute,
-      builder: () => NativeMessagingScreen(),
-    )
+    // QRoute(
+    //   name: kAgoraCallScreenRoute,
+    //   path: kAgoraCallScreenRoute,
+    //   builder: () => AgoraCall(),
+    // ),
+    // QRoute(
+    //   name: SharedRoutes.kMessagesRoute,
+    //   path: SharedRoutes.kMessagesRoute,
+    //   builder: () => NativeMessagingScreen(),
+    // )
   ];
 }
