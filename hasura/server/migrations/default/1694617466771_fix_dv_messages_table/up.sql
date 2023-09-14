f4dd8c723a6a0df556797c02a23e5d1f8e69b950
Montassare Zekri <montassare6@gmail.com>
2023-09-14 09:10:23 +0100

    fix migrations ducplicate

--- hasura/server/migrations/default/1694617466771_fix_dv_messages_table/up.sql
+++ hasura/server/migrations/default/1694617466771_fix_dv_messages_table/up.sql
@@ -1,7 +1,4 @@
 
-alter table "delivery"."details" add column "sitin_available" boolean
- not null default 'true';
-
 alter table "public"."delivery_messages" rename column "resolved_time" to "finished_time";
 
 alter table "public"."delivery_messages" add column "responded_time" Timestamp 
